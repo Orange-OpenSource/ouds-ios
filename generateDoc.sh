8a1cef5a00f98e9f082f74a0f39410fe60fb4022
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2024-09-11 19:04:30 +0200

    doc: stylesheet for chapeau page
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- generateDoc.sh
+++ generateDoc.sh
@@ -42,7 +42,6 @@ TARGETS="OUDS \
 # Step 1 - For each target, build the DocC documentation
 # ------------------------------------------------------
 
-# Start timer (for elapsed time computation)
 start_time=$(date +%s)
 
 echo "🍊👉 Generating docs..."
@@ -75,20 +74,21 @@ echo "🍊👍 Updated!"
 
 echo "🍊👉 Updating index.html..."
 
-# TODO: Stylesheet
-
-echo "<!DOCTYPE html><html><head></head><body><ol>" > $DOCS_DIRECTORY/index.html
-
+echo "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>OUDS iOS Swift Documentation</title><style>body{font-family:Arial,sans-serif;line-height:1.6;margin:0;padding:0;background-color:#000000;color:#fff;}header{background-color:#424242;color:white;padding:20px;text-align:center;}main{padding:20px;}h1{margin:0;}ol{padding-left:20px;}li{margin:10px 0;}a{color:#06f;text-decoration:none;}a:hover{text-decoration:underline;}footer{text-align:center;padding:10px;background-color:#000000;border-top:1px solid #e0e0e0;}</style></head><body><header><h1>OUDS iOS Swift Documentation/h1></header><main>" > $DOCS_DIRECTORY/index.html
+echo "<h2>iOS SwiftUI implementation of the Unified Design System of Orange</h2>" >> $DOCS_DIRECTORY/index.html
+echo "<h3>All targets of the Swift Package are listed below</h3>" >> $DOCS_DIRECTORY/index.html
+echo "<ol>" >> $DOCS_DIRECTORY/index.html
 for target in ${TARGETS}
 do
-    cp -R $target-docs/data/documentation/* $DOCS_DIRECTORY/data/documentation/
-    cp -R $target-docs/documentation/* $DOCS_DIRECTORY/documentation/
+    cp -R "$target-docs/data/documentation/"* "$DOCS_DIRECTORY/data/documentation/"
+    cp -R "$target-docs/documentation/"* "$DOCS_DIRECTORY/documentation/"
     rm -r "$target-docs"
-    modified_target=$(echo $target | tr '-' '_' | tr '[:upper:]' '[:lower:]')
+    modified_target=$(echo "$target" | tr '-' '_' | tr '[:upper:]' '[:lower:]')
     echo "<li><a href=\"./documentation/$modified_target\">$target</a></li>" >> $DOCS_DIRECTORY/index.html
 done
-
-echo "</ol></body></html>" >> $DOCS_DIRECTORY/index.html
+echo "</ol></main>" >> $DOCS_DIRECTORY/index.html
+echo "<footer><p>Find the source code on <a href=\"https://github.com/Orange-OpenSource/ouds-ios\">GitHub</a></p>" >> $DOCS_DIRECTORY/index.html
+echo "<p>&copy; $(date +%Y) Orange</p></footer></body></html>" >> $DOCS_DIRECTORY/index.html
 
 echo "🍊👍 index.html updated!"
 
@@ -121,12 +121,12 @@ git push origin
 echo "🍊🔨 Cleaning stashes"
 git stash clear
 
-commitHash=`git rev-parse HEAD`
+commit_hash=`git rev-parse HEAD`
 
 echo "🍊🔨 Going back to previous Git branch"
 git checkout "$current_branch"
 
-echo "🍊👍 Pushed with commit '$commitHash'"
+echo "🍊👍 Pushed with commit '$commit_hash'"
 echo "🍊🎉 Documentation also available in $DOCS_DIRECTORY"
 echo "🍊🧮 There are '$files_count' in $DOCS_DIRECTORY!"
 
@@ -136,6 +136,6 @@ echo "🍊👍 Documentation ZIP available at $DOCUMENTATION_ZIP_LOCATION"
 
 end_time=$(date +%s)
 elapsed_time=$(( end_time - start_time ))
-elapsed_time_minutes=$(( elapsed / 60 ))
-elapsed_time_seconds=$(( elapsed % 60 ))
-echo "🍊⌛ Elapsed time: ${minutes} minutes and ${seconds} seconds"
+elapsed_time_minutes=$(( elapsed_time / 60 ))
+elapsed_time_seconds=$(( elapsed_time % 60 ))
+echo "🍊⌛ Elapsed time: ${elapsed_time_minutes} minutes and ${elapsed_time_seconds} seconds"
