eafefe5a077eb0ce9f27fa4a0c47862f80a3eefa
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2024-09-11 15:05:26 +0200

    chore: build doc - WIP
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- generateDoc.sh
+++ generateDoc.sh
@@ -12,13 +12,22 @@
 # Software description: A SwiftUI components library with code examples for Orange Unified Design System
 #
 
+# Controls on script
 set -euo pipefail
 
-echo "👉 Generating docs..."
+# The name of the Git branch hosting GitHub Pages service
+GH_PAGES_BRANCH="gh-pages"
 
-DOCS_DIRECTORY=./docs
-mkdir -p ${DOCS_DIRECTORY}
+# The generated name of the ZIP containing the generated sources of documentation (for archive)
+timestamp=$(date +%s)
+DOCUMENTATION_ZIP_NAME="documentation-$timestamp.zip"
+DOCUMENTATION_ZIP_LOCATION="/tmp/$DOCUMENTATION_ZIP_NAME"
 
+# Path where the documentation will be
+DOCS_DIRECTORY="./docs"
+mkdir -p "$DOCS_DIRECTORY"
+
+# The targets to build documentation of
 TARGETS="OUDS \
         OUDSThemesSosh \
         OUDSThemesInverse \
@@ -30,34 +39,103 @@ TARGETS="OUDS \
         OUDSModules \
         OUDSFoundations"
 
+# Step 1 - For each target, build the DocC documentation
+# ------------------------------------------------------
+
+# Start timer (for elapsed time computation)
+start_time=$(date +%s)
+
+echo "🍊👉 Generating docs..."
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+echo "🍊🔨 Cleaning Git repository before generation (current Git branch is '$current_branch')"
+git reset --hard
+git clean -fd
+
 for target in ${TARGETS}
 do
-    echo "👉 Generating docs for $target..."
+    echo "🍊👉 Generating docs for $target..."
     swift package --allow-writing-to-directory "$target-docs" generate-documentation  --disable-indexing --transform-for-static-hosting --output-path "$target-docs" --target "$target"
     
-    cp -r $target-docs/* ${DOCS_DIRECTORY}
+    cp -r $target-docs/* $DOCS_DIRECTORY
     modified_target=$(echo $target | tr '-' '_' | tr '[:upper:]' '[:lower:]')
-    cp -r $target-docs/index/index.json "${DOCS_DIRECTORY}/index/$modified_target.json"
-    echo "👍 Docs generated for $target!"
+    cp -r $target-docs/index/index.json "$DOCS_DIRECTORY/index/$modified_target.json"
+    echo "🍊👍 Docs generated for $target!"
 done
 
-echo "👉 Updating index.html..."
+# Step 2 - Add CNAME file for GitHub Pages
+# ----------------------------------------
 
-echo "<!DOCTYPE html><html><head></head><body><ol>" > ${DOCS_DIRECTORY}/index.html
+echo "🍊👉 Updating CNAME file"
+echo "ios.unified-design-system.orange.com" > "$DOCS_DIRECTORY/CNAME"
+echo "🍊👍 Updated!"
+    
+# Step 3 - Update global HTML file
+# --------------------------------
+
+echo "🍊👉 Updating index.html..."
+
+# TODO: Stylesheet
+
+echo "<!DOCTYPE html><html><head></head><body><ol>" > $DOCS_DIRECTORY/index.html
 
 for target in ${TARGETS}
 do
-    cp -R $target-docs/data/documentation/* ${DOCS_DIRECTORY}/data/documentation/
-    cp -R $target-docs/documentation/* ${DOCS_DIRECTORY}/documentation/
+    cp -R $target-docs/data/documentation/* $DOCS_DIRECTORY/data/documentation/
+    cp -R $target-docs/documentation/* $DOCS_DIRECTORY/documentation/
     rm -r "$target-docs"
     modified_target=$(echo $target | tr '-' '_' | tr '[:upper:]' '[:lower:]')
-    echo "<li><a href=\"./documentation/$modified_target\">$target</a></li>" >> ${DOCS_DIRECTORY}/index.html
+    echo "<li><a href=\"./documentation/$modified_target\">$target</a></li>" >> $DOCS_DIRECTORY/index.html
 done
 
-echo "</ol></body></html>" >> ${DOCS_DIRECTORY}/index.html
+echo "</ol></body></html>" >> $DOCS_DIRECTORY/index.html
 
-echo "👍 index.html updated!"
+echo "🍊👍 index.html updated!"
 
-echo "👍 Docs generated!"
+# Step 4 - Checkout to GitHub Pages dedicated branch
+# --------------------------------------------------
 
-echo "🎉 Documentation available in ${DOCS_DIRECTORY}"
+echo "🍊👉 Versioning documentation in GitHub Pages branch (it can take a lot of time)..."
+
+echo "🍊🔨 Stashing things"
+git stash -u
+
+echo "🍊🔨 Checkout GitHub Pages branch"
+git checkout $GH_PAGES_BRANCH
+git reset --hard origin/$GH_PAGES_BRANCH
+
+echo "🍊🔨 Unstashing things"
+git stash apply
+
+echo "🍊🔨 Adding things"
+git add $DOCS_DIRECTORY
+
+files_count=`find $DOCS_DIRECTORY -type f | wc -l | xargs`
+
+echo "🍊🔨 Committing things (be ready if passwords are asked)"
+git commit -m $'doc: update DocC documentation for GitHub Pages\n\nWARNING: This is an automatic commit 🤖'
+
+echo "🍊🔨 Pushing things"
+git push origin
+
+echo "🍊🔨 Cleaning stashes"
+git stash clear
+
+commitHash=`git rev-parse HEAD`
+
+echo "🍊🔨 Going back to previous Git branch"
+git checkout "$current_branch"
+
+echo "🍊👍 Pushed with commit '$commitHash'"
+echo "🍊🎉 Documentation also available in $DOCS_DIRECTORY"
+echo "🍊🧮 There are '$files_count' in $DOCS_DIRECTORY!"
+
+echo "🍊👉 Zipping documentation folder"
+zip -r "$DOCUMENTATION_ZIP_LOCATION"  "$DOCS_DIRECTORY"
+echo "🍊👍 Documentation ZIP available at $DOCUMENTATION_ZIP_LOCATION"
+
+end_time=$(date +%s)
+elapsed_time=$(( end_time - start_time ))
+elapsed_time_minutes=$(( elapsed / 60 ))
+elapsed_time_seconds=$(( elapsed % 60 ))
+echo "🍊⌛ Elapsed time: ${minutes} minutes and ${seconds} seconds"
