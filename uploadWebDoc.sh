99b9ad2229a7f724da3870b344c0d3016eaa4b43
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2024-10-17 16:22:59 +0200

    chore: update help message of documentation script
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- uploadWebDoc.sh
+++ uploadWebDoc.sh
@@ -101,7 +101,7 @@ clean_repo() {
 }
 
 show_help() {
-    echo "This script will use swift-docc-plugin to build HTML documentation of the OUDS iOS Swift Package"
+    echo "This script will pick HTML files in some doccarchive folders to build HTML documentation of the OUDS iOS Swift Package"
     echo "It can also update the Git repository."
     echo -e "Usage: $0 [--help] --libversion=VERSION [--usegit | --nozip]\n"
     echo "Options:"
