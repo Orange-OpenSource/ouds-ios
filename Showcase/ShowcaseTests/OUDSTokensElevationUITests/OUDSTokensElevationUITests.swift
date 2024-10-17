e528759e056a7a3af7623c3fed764e96108cfbf3
Tayeb Sedraia <tayeb.sedraia@orange.com>
2024-10-17 17:19:38 +0200

    review : adjust test class names for elevation

--- Showcase/ShowcaseTests/OUDSTokensElevationUITests/OUDSTokensElevationUITests.swift
+++ Showcase/ShowcaseTests/OUDSTokensElevationUITests/OUDSTokensElevationUITests.swift
@@ -27,77 +27,77 @@ class OUDSTokensElevationUITests: XCTestCase {
     // MARK: - Tests
     // MARK: - Orange Theme Light Mode Tests
     func testElevationToken_OrangeTheme_ElevationNone_Light() {
-        let vc = BorderTokenPage().environment(\.theme, OrangeTheme())
+        let vc = ElevationTokenPage().environment(\.theme, OrangeTheme())
         assertSnapshot(of: vc, as: .image, named: "OrangeTheme/testElevationToken_OrangeTheme_ElevationNone_Light")
     }
     func testElevationToken_OrangeTheme_ElevationFocus_Light() {
-        let vc = BorderTokenPage().environment(\.theme, OrangeTheme())
+        let vc = ElevationTokenPage().environment(\.theme, OrangeTheme())
         assertSnapshot(of: vc, as: .image, named: "OrangeTheme/testElevationToken_OrangeTheme_ElevationFocus_Light")
     }
     func testElevationToken_OrangeTheme_ElevationRaised_Light() {
-        let vc = BorderTokenPage().environment(\.theme, OrangeTheme())
+        let vc = ElevationTokenPage().environment(\.theme, OrangeTheme())
         assertSnapshot(of: vc, as: .image, named: "OrangeTheme/testElevationToken_OrangeTheme_ElevationRaised_Light")
     }
     func testElevationToken_OrangeTheme_ElevationStickyNavigationScrolled_Light() {
-        let vc = BorderTokenPage().environment(\.theme, OrangeTheme())
+        let vc = ElevationTokenPage().environment(\.theme, OrangeTheme())
         assertSnapshot(of: vc, as: .image, named: "OrangeTheme/testElevationToken_OrangeTheme_ElevationStickyNavigationScrolled_Light")
     }
     func testElevationToken_OrangeTheme_ElevationOverlayDefault_Light() {
-        let vc = BorderTokenPage().environment(\.theme, OrangeTheme())
+        let vc = ElevationTokenPage().environment(\.theme, OrangeTheme())
         assertSnapshot(of: vc, as: .image, named: "OrangeTheme/testElevationToken_OrangeTheme_ElevationOverlayDefault_Light")
     }
     func testElevationToken_OrangeTheme_ElevationStickyDefault_Light() {
-        let vc = BorderTokenPage().environment(\.theme, OrangeTheme())
+        let vc = ElevationTokenPage().environment(\.theme, OrangeTheme())
         assertSnapshot(of: vc, as: .image, named: "OrangeTheme/testElevationToken_OrangeTheme_ElevationStickyDefault_Light")
     }
     func testElevationToken_OrangeTheme_ElevationStickyEmphasized_Light() {
-        let vc = BorderTokenPage().environment(\.theme, OrangeTheme())
+        let vc = ElevationTokenPage().environment(\.theme, OrangeTheme())
         assertSnapshot(of: vc, as: .image, named: "OrangeTheme/testElevationToken_OrangeTheme_ElevationStickyEmphasized_Light")
     }
     func testElevationToken_OrangeTheme_ElevationDrag_Light() {
-        let vc = BorderTokenPage().environment(\.theme, OrangeTheme())
+        let vc = ElevationTokenPage().environment(\.theme, OrangeTheme())
         assertSnapshot(of: vc, as: .image, named: "OrangeTheme/testElevationToken_OrangeTheme_ElevationDrag_Light")
     }
     func testElevationToken_OrangeTheme_ElevationOverlayEmphasized_Light() {
-        let vc = BorderTokenPage().environment(\.theme, OrangeTheme())
+        let vc = ElevationTokenPage().environment(\.theme, OrangeTheme())
         assertSnapshot(of: vc, as: .image, named: "OrangeTheme/testElevationToken_OrangeTheme_ElevationOverlayEmphasized_Light")
     }
     // MARK: - Tests
     // MARK: - Inverse Theme Light Mode Tests
     func testElevationToken_InverseTheme_ElevationNone_Light() {
-        let vc = BorderTokenPage().environment(\.theme, InverseTheme())
+        let vc = ElevationTokenPage().environment(\.theme, InverseTheme())
         assertSnapshot(of: vc, as: .image, named: "InverseTheme/testElevationToken_InverseTheme_ElevationNone_Light")
     }
     func testElevationToken_InverseTheme_ElevationFocus_Light() {
-        let vc = BorderTokenPage().environment(\.theme, InverseTheme())
+        let vc = ElevationTokenPage().environment(\.theme, InverseTheme())
         assertSnapshot(of: vc, as: .image, named: "InverseTheme/testElevationToken_InverseTheme_ElevationFocus_Light")
     }
     func testElevationToken_InverseTheme_ElevationRaised_Light() {
-        let vc = BorderTokenPage().environment(\.theme, InverseTheme())
+        let vc = ElevationTokenPage().environment(\.theme, InverseTheme())
         assertSnapshot(of: vc, as: .image, named: "InverseTheme/testElevationToken_InverseTheme_ElevationRaised_Light")
     }
     func testElevationToken_InverseTheme_ElevationStickyNavigationScrolled_Light() {
-        let vc = BorderTokenPage().environment(\.theme, InverseTheme())
+        let vc = ElevationTokenPage().environment(\.theme, InverseTheme())
         assertSnapshot(of: vc, as: .image, named: "InverseTheme/testElevationToken_InverseTheme_ElevationStickyNavigationScrolled_Light")
     }
     func testElevationToken_InverseTheme_ElevationOverlayDefault_Light() {
-        let vc = BorderTokenPage().environment(\.theme, InverseTheme())
+        let vc = ElevationTokenPage().environment(\.theme, InverseTheme())
         assertSnapshot(of: vc, as: .image, named: "InverseTheme/testElevationToken_InverseTheme_ElevationOverlayDefault_Light")
     }
     func testElevationToken_InverseTheme_ElevationStickyDefault_Light() {
-        let vc = BorderTokenPage().environment(\.theme, InverseTheme())
+        let vc = ElevationTokenPage().environment(\.theme, InverseTheme())
         assertSnapshot(of: vc, as: .image, named: "InverseTheme/testElevationToken_InverseTheme_ElevationStickyDefault_Light")
     }
     func testElevationToken_InverseTheme_ElevationStickyEmphasized_Light() {
-        let vc = BorderTokenPage().environment(\.theme, InverseTheme())
+        let vc = ElevationTokenPage().environment(\.theme, InverseTheme())
         assertSnapshot(of: vc, as: .image, named: "InverseTheme/testElevationToken_InverseTheme_ElevationStickyEmphasized_Light")
     }
     func testElevationToken_InverseTheme_ElevationDrag_Light() {
-        let vc = BorderTokenPage().environment(\.theme, InverseTheme())
+        let vc = ElevationTokenPage().environment(\.theme, InverseTheme())
         assertSnapshot(of: vc, as: .image, named: "InverseTheme/testElevationToken_InverseTheme_ElevationDrag_Light")
     }
     func testElevationToken_InverseTheme_ElevationOverlayEmphasized_Light() {
-        let vc = BorderTokenPage().environment(\.theme, InverseTheme())
+        let vc = ElevationTokenPage().environment(\.theme, InverseTheme())
         assertSnapshot(of: vc, as: .image, named: "InverseTheme/testElevationToken_InverseTheme_ElevationOverlayEmphasized_Light")
     }
 }
