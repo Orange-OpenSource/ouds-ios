9f5d0d959045809bfa8067375f5e2e4ad04b14b1
Tayeb Sedraia <tayeb.sedraia@orange.com>
2024-10-18 15:46:19 +0200

    review : fix consistency with SwiftLint rules

--- Showcase/ShowcaseTests/OUDSComponentsUITests/OUDSComponentsUITests.swift
+++ Showcase/ShowcaseTests/OUDSComponentsUITests/OUDSComponentsUITests.swift
@@ -11,29 +11,36 @@
 // Software description: A SwiftUI components library with code examples for Orange Unified Design System 
 //
 
-import SwiftUI
-import XCTest
-
 import OUDS
 import OUDSComponents
 import OUDSThemesInverse
 import OUDSThemesOrange
 import OUDSTokensSemantic
 import SnapshotTesting
+import SwiftUI
+import XCTest
+
+// swiftlint:disable required_deinit
+
+/// To ensure the Components Screen  are tested for UI compatibility with the reference image previously recorded.
 
 class OUDSComponentsUITests: XCTestCase {
-    deinit {
-    }
-    // MARK: - Tests
+
     // MARK: - Orange Theme Light Mode Components Page Tests
+
+    /// Verifies that the components from the orange theme's page match the saved reference image in light mode using snapshot testing.
     func testComponents_OrangeTheme_Page_Light() {
         let vc = ComponentsPage().environment(\.theme, OrangeTheme())
         assertSnapshot(of: vc, as: .image, named: "OrangeTheme/testComponents_OrangeTheme_Page_Light")
     }
-    // MARK: - Tests
+
     // MARK: - Inverse Theme Light Mode Components Page Tests
+
+    /// Verifies that the components from the inverse theme's page match the saved reference image in light mode using snapshot testing.
     func testComponents_InverseTheme_Page_Light() {
         let vc = ComponentsPage().environment(\.theme, InverseTheme())
         assertSnapshot(of: vc, as: .image, named: "OrangeTheme/testComponents_InverseTheme_Page_Light")
     }
 }
+
+// swiftlint:enable required_deinit
