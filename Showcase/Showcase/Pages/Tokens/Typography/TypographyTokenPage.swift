ede91b2a604a5904c58e272247bce9673892e88e
LudovicPinel <ludovic.pinel@orange.com>
2024-10-24 17:12:42 +0200

    refactor: typography screen to use right color on token name (#213) (#244)
    Reviewed-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- Showcase/Showcase/Pages/Tokens/Typography/TypographyTokenPage.swift
+++ Showcase/Showcase/Pages/Tokens/Typography/TypographyTokenPage.swift
@@ -31,7 +31,7 @@ struct TypographyTokenPage: View {
             }
         }
         .frame(maxWidth: .infinity, alignment: .leading)
-        .padding(.horizontal, theme.spaceFixedShort)
+        .padding(.horizontal, theme.spaceFixedMedium)
         .navigationTitle(LocalizedStringKey("app_tokens_typography_label"))
     }
 
@@ -43,7 +43,7 @@ struct TypographyTokenPage: View {
 
         VStack(alignment: .leading, spacing: theme.spaceFixedNone) {
             typgraphyIllustration(from: namedTypography)
-                .foregroundStyle(theme.colorContentMuted.color(for: colorScheme))
+                .foregroundStyle(theme.colorContentDefault.color(for: colorScheme))
 
             Group {
                 Text("family (\(theme.customFontFamily ?? "system")), ")
@@ -54,7 +54,7 @@ struct TypographyTokenPage: View {
             }
             .typeBodyDefaultMedium(theme)
             .fixedSize(horizontal: false, vertical: true)
-            .foregroundStyle(theme.colorContentDefault.color(for: colorScheme))
+            .foregroundStyle(theme.colorContentMuted.color(for: colorScheme))
         }
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding(.vertical, theme.spaceFixedShorter)
@@ -64,52 +64,58 @@ struct TypographyTokenPage: View {
     private func typgraphyIllustration(from namedTypography: NamedTypography) -> some View {
         switch namedTypography {
         case .displayLarge:
-            Text(namedTypography.rawValue).typeDisplayLarge(theme)
+            Text(namedTypography.rawValue.camelCase).typeDisplayLarge(theme)
         case .displayMedium:
-            Text(namedTypography.rawValue).typeDisplayMedium(theme)
+            Text(namedTypography.rawValue.camelCase).typeDisplayMedium(theme)
         case .displaySmall:
-            Text(namedTypography.rawValue).typeDisplaySmall(theme)
+            Text(namedTypography.rawValue.camelCase).typeDisplaySmall(theme)
         case .headingXLarge:
-            Text(namedTypography.rawValue).typeHeadingXLarge(theme)
+            Text(namedTypography.rawValue.camelCase).typeHeadingXLarge(theme)
         case .headingLarge:
-            Text(namedTypography.rawValue).typeHeadingLarge(theme)
+            Text(namedTypography.rawValue.camelCase).typeHeadingLarge(theme)
         case .headingMedium:
-            Text(namedTypography.rawValue).typeHeadingMedium(theme)
+            Text(namedTypography.rawValue.camelCase).typeHeadingMedium(theme)
         case .headingSmall:
-            Text(namedTypography.rawValue).typeHeadingSmall(theme)
+            Text(namedTypography.rawValue.camelCase).typeHeadingSmall(theme)
         case .bodyDefaultLarge:
-            Text(namedTypography.rawValue).typeBodyDefaultLarge(theme)
+            Text(namedTypography.rawValue.camelCase).typeBodyDefaultLarge(theme)
         case .bodyDefaultMedium:
-            Text(namedTypography.rawValue).typeBodyDefaultMedium(theme)
+            Text(namedTypography.rawValue.camelCase).typeBodyDefaultMedium(theme)
         case .bodyDefaultSmall:
-            Text(namedTypography.rawValue).typeBodyDefaultSmall(theme)
+            Text(namedTypography.rawValue.camelCase).typeBodyDefaultSmall(theme)
         case .bodyStrongLarge:
-            Text(namedTypography.rawValue).typeBodyStrongLarge(theme)
+            Text(namedTypography.rawValue.camelCase).typeBodyStrongLarge(theme)
         case .bodyStrongMedium:
-            Text(namedTypography.rawValue).typeBodyStrongMedium(theme)
+            Text(namedTypography.rawValue.camelCase).typeBodyStrongMedium(theme)
         case .bodyStrongSmall:
-            Text(namedTypography.rawValue).typeBodyStrongSmall(theme)
+            Text(namedTypography.rawValue.camelCase).typeBodyStrongSmall(theme)
         case .labelDefaultXLarge:
-            Text(namedTypography.rawValue).typeLabelDefaultXLarge(theme)
+            Text(namedTypography.rawValue.camelCase).typeLabelDefaultXLarge(theme)
         case .labelDefaultLarge:
-            Text(namedTypography.rawValue).typeLabelDefaultLarge(theme)
+            Text(namedTypography.rawValue.camelCase).typeLabelDefaultLarge(theme)
         case .labelDefaultMedium:
-            Text(namedTypography.rawValue).typeLabelDefaultMedium(theme)
+            Text(namedTypography.rawValue.camelCase).typeLabelDefaultMedium(theme)
         case .labelDefaultSmall:
-            Text(namedTypography.rawValue).typeLabelDefaultSmall(theme)
+            Text(namedTypography.rawValue.camelCase).typeLabelDefaultSmall(theme)
         case .labelStrongXLarge:
-            Text(namedTypography.rawValue).typeLabelStrongXLarge(theme)
+            Text(namedTypography.rawValue.camelCase).typeLabelStrongXLarge(theme)
         case .labelStrongLarge:
-            Text(namedTypography.rawValue).typeLabelStrongLarge(theme)
+            Text(namedTypography.rawValue.camelCase).typeLabelStrongLarge(theme)
         case .labelStrongMedium:
-            Text(namedTypography.rawValue).typeLabelStrongMedium(theme)
+            Text(namedTypography.rawValue.camelCase).typeLabelStrongMedium(theme)
         case .labelStrongSmall:
-            Text(namedTypography.rawValue).typeLabelStrongSmall(theme)
+            Text(namedTypography.rawValue.camelCase).typeLabelStrongSmall(theme)
         case .codeSmall:
-            Text(namedTypography.rawValue).typeCodeMedium(theme)
+            Text(namedTypography.rawValue.camelCase).typeCodeMedium(theme)
         case .codeMedium:
-            Text(namedTypography.rawValue).typeCodeMedium(theme)
+            Text(namedTypography.rawValue.camelCase).typeCodeMedium(theme)
         }
+    }
+}
+
+extension String {
+    var camelCase: String {
+        self.prefix(1).capitalized + self.dropFirst()
     }
 }
 
