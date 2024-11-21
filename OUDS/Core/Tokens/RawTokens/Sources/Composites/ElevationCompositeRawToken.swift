f8cc5e2bbfab1764ce58ed53017938bdf347ee30
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2024-11-21 11:57:17 +0100

    docs: update DocC comments and catalog for raw tokens (#279)
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- OUDS/Core/Tokens/RawTokens/Sources/Composites/ElevationCompositeRawToken.swift
+++ OUDS/Core/Tokens/RawTokens/Sources/Composites/ElevationCompositeRawToken.swift
@@ -13,7 +13,10 @@
 
 import Foundation
 
-/// In the global design system, composite tokens are defined for elevation effects.
+/// In the *Figma* global design system, composite tokens are defined for elevation effects.
+/// Composite tokens are tokens defined in *Figma* by other tokens.
+/// Here an *elevation* is caracterized by *x and Y offsets*, a *radius* for the shadow and a *color* to apply on that shadow.
+/// All these elements are *raw tokens*, and together define a *composite raw token* for *elevation* effect.
 /// It is defined as a `final class` and `NSObject` so as to be shared through `@objc` with extensions and protocols within modules.
 public final class ElevationCompositeRawToken: NSObject, Sendable { // NSObject for @objc compatibility
 
--- OUDS/Core/Tokens/RawTokens/Sources/Declarations/ElevationRawTokens.swift
+++ OUDS/Core/Tokens/RawTokens/Sources/Declarations/ElevationRawTokens.swift
@@ -12,6 +12,10 @@
 //
 
 /// Defined as the group of all **raw tokens** related to **elevations**.
+/// Values are defined in *ElevationRawTokens+Values.swift* file updated by the tokenator and *ElevationRawTokens+Values.swift* updated manualy by the development team.
+/// `ElevationRawTokens` are tokens to use to define *elevations* , i.e. in the end box shadow effects. These tokens are used to define these effects thanks to several types: the *blur*, the *X offset* and the *Y offset*, all of them being `ElevationRawToken` type.
+/// Combinations of these three values can be defined inside `ElevationCompositeRawToken` i.e. *composite tokens* defining a whil box shadow to use for elevation effect.
+/// 
 /// Primitive types such as `CGFloat` must be used to as to allow to use `@objc` keywords in extensions for overriding.
 /// Such tokens are packed in a _Swift enum_ so as to gather them in one object with the suitable namespace and avoid to have just constants in nothing else
 ///  (i.e. publicly accessible from everywhere). More optimized than _struct_.
@@ -19,4 +23,5 @@ public enum ElevationRawTokens {
 
     // ଘ( ･ω･)_/ﾟ･:*:･｡☆
     // Note: So as to help the Figma-JSON-to-Swift parser the values have been added in the Values folder
+    // This trick helps also to write documentation for these tokens as the tokenator cannot generate documentation
 }
