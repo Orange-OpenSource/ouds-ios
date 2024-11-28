978f97b1fc9e9b1b6c46a6afec92a25d2280e43e
Ludovic PINEL <ludovic.pinel@orange.com>
2024-11-28 15:48:32 +0100

    Update size tests

--- Showcase/ShowcaseTests/__Snapshots__/OUDSTokensDimensionUITests/Size/OUDSTokensSizeUITests.swift
+++ Showcase/ShowcaseTests/__Snapshots__/OUDSTokensDimensionUITests/Size/OUDSTokensSizeUITests.swift
@@ -25,166 +25,105 @@ import XCTest
 /// Tests the UI rendering of each **size token** using reference images
 final class OUDSTokensSizeUITests: XCTestCase {
 
-    // MARK: Properties
-
-    private let inverseTheme = InverseTheme()
-    private let orangeTheme = OrangeTheme()
-    private let lightScheme: ColorScheme = .light
-    private let darkScheme: ColorScheme = .dark
-
-    // MARK: - Orange Theme Light Mode Dimension Tests
+    // MARK: - Orange Theme Light Mode Size Tests
 
     /// This function tests all size tokens in the `OrangeTheme` with the `light` color scheme.
     /// It iterates through all `NamedSize` cases, rendering each illustration in a `UIHostingController`
-    /// and captures a snapshot. The snapshot is saved with a name indicating the size, theme, and color scheme.
+    /// and captures a snapshot. The snapshot is saved with a name indicating the elevation, theme, and color scheme.
     @MainActor func testAllSizesOrangeThemeLight() {
-        // IconDecorative
-        for sizing in NamedSize.IconDecorative.allCases {
-            // Use the `IllustrationIconDecorative` struct to test a single illustration
-            let illustration = OUDSThemeableView(theme: orangeTheme) {
-                SizeTokenPage.IllustrationIconDecorative(namedSize: sizing)
-                    .background(self.orangeTheme.colorBgPrimary.color(for: self.lightScheme))
-            }
-
-            // Encapsulate the element in a UIHostingController for snapshot testing
-            let hostingVC = UIHostingController(rootView: illustration)
-
-            // Capture the snapshot of the illustration with the correct theme and color scheme
-            let snapshotName = "\(sizing.rawValue)"
-            assertSnapshot(of: hostingVC, as: .image(traits: UITraitCollection(userInterfaceStyle: .light)), named: snapshotName)
-        }
-
-        // IconWithLabel
-        for sizing in NamedSize.IconWithTypography.allCases {
-            // Use the `IllustrationIconWithLabel` struct to test a single illustration
-            let illustration = OUDSThemeableView(theme: orangeTheme) {
-                SizeTokenPage.IllustrationIconWithLabel(namedSize: sizing)
-                    .background(self.orangeTheme.colorBgPrimary.color(for: self.lightScheme))
-            }
-
-            // Encapsulate the element in a UIHostingController for snapshot testing
-            let hostingVC = UIHostingController(rootView: illustration)
-
-            // Capture the snapshot of the illustration with the correct theme and color scheme
-            let snapshotName = "\(sizing.rawValue)"
-            assertSnapshot(of: hostingVC, as: .image(traits: UITraitCollection(userInterfaceStyle: .light)), named: snapshotName)
-        }
+        let theme = OrangeTheme()
+        let interfaceStyle = UIUserInterfaceStyle.light
+        testSizeIconDecorative(for: theme, in: interfaceStyle)
+        testSizeIconWithTypography(for: theme, in: interfaceStyle)
     }
 
-    // MARK: - Orange Theme Dark Mode Border Tests
+    // MARK: - Orange Theme Light Mode Size Tests
 
     /// This function tests all size tokens in the `OrangeTheme` with the `dark` color scheme.
-    /// It iterates through all `NamedSize`cases, rendering each illustration in a `UIHostingController`
-    /// and captures a snapshot. The snapshot is saved with a name indicating the size, theme, and color scheme.
-    @MainActor func testAllSizesOrangeThemeDark() {
-        // IconDecorative
-        for sizing in NamedSize.IconDecorative.allCases {
-            // Use the `IllustrationIconDecorative` struct to test a single illustration
-            let illustration = OUDSThemeableView(theme: orangeTheme) {
-                SizeTokenPage.IllustrationIconDecorative(namedSize: sizing)
-                    .background(self.orangeTheme.colorBgPrimary.color(for: self.darkScheme))
-            }
-
-            // Encapsulate the element in a UIHostingController for snapshot testing
-            let hostingVC = UIHostingController(rootView: illustration)
-
-            // Capture the snapshot of the illustration with the correct theme and color scheme
-            let snapshotName = "\(sizing.rawValue)"
-            assertSnapshot(of: hostingVC, as: .image(traits: UITraitCollection(userInterfaceStyle: .dark)), named: snapshotName)
-        }
-
-        // IconWithLabel
-        for sizing in NamedSize.IconWithTypography.allCases {
-            // Use the `IllustrationIconWithLabel` struct to test a single illustration
-            let illustration = OUDSThemeableView(theme: orangeTheme) {
-                SizeTokenPage.IllustrationIconWithLabel(namedSize: sizing)
-                    .background(self.orangeTheme.colorBgPrimary.color(for: self.darkScheme))
-            }
-
-            // Encapsulate the element in a UIHostingController for snapshot testing
-            let hostingVC = UIHostingController(rootView: illustration)
-
-            // Capture the snapshot of the illustration with the correct theme and color scheme
-            let snapshotName = "\(sizing.rawValue)"
-            assertSnapshot(of: hostingVC, as: .image(traits: UITraitCollection(userInterfaceStyle: .dark)), named: snapshotName)
-        }
-    }
-
-    // MARK: - Inverse Theme Light Mode Border Tests
-
-    /// This function tests all size tokens in the `InverseTheme` with the `light` color scheme.
     /// It iterates through all `NamedSize` cases, rendering each illustration in a `UIHostingController`
-    /// and captures a snapshot. The snapshot is saved with a name indicating the size, theme, and color scheme.
-    @MainActor func testAllSizesInverseThemeLight() {
-        // IconDecorative
-        for sizing in NamedSize.IconDecorative.allCases {
-            // Use the `IllustrationIconDecorative` struct to test a single illustration
-            let illustration = OUDSThemeableView(theme: inverseTheme) {
-                SizeTokenPage.IllustrationIconDecorative(namedSize: sizing)
-                    .background(self.inverseTheme.colorBgPrimary.color(for: self.lightScheme))
-            }
-
-            // Encapsulate the element in a UIHostingController for snapshot testing
-            let hostingVC = UIHostingController(rootView: illustration)
-
-            // Capture the snapshot of the illustration with the correct theme and color scheme
-            let snapshotName = "\(sizing.rawValue)"
-            assertSnapshot(of: hostingVC, as: .image(traits: UITraitCollection(userInterfaceStyle: .light)), named: snapshotName)
-        }
-
-        // IconWithLabel
-        for sizing in NamedSize.IconWithTypography.allCases {
-            // Use the `IllustrationIconWithLabel` struct to test a single illustration
-            let illustration = OUDSThemeableView(theme: inverseTheme) {
-                SizeTokenPage.IllustrationIconWithLabel(namedSize: sizing)
-                    .background(self.inverseTheme.colorBgPrimary.color(for: self.lightScheme))
-            }
-
-            // Encapsulate the element in a UIHostingController for snapshot testing
-            let hostingVC = UIHostingController(rootView: illustration)
-
-            // Capture the snapshot of the illustration with the correct theme and color scheme
-            let snapshotName = "\(sizing.rawValue)"
-            assertSnapshot(of: hostingVC, as: .image(traits: UITraitCollection(userInterfaceStyle: .light)), named: snapshotName)
-        }
+    /// and captures a snapshot. The snapshot is saved with a name indicating the elevation, theme, and color scheme.
+    @MainActor func testAllSizesOrangeThemeDark() {
+        let theme = OrangeTheme()
+        let interfaceStyle = UIUserInterfaceStyle.dark
+        testSizeIconDecorative(for: theme, in: interfaceStyle)
+        testSizeIconWithTypography(for: theme, in: interfaceStyle)
     }
 
-    // MARK: - Inverse Theme Dark Mode Border Tests
+    // MARK: - Inverse Theme Light Mode Size Tests
+
+    /// This function tests all sizes tokens in the `InverseTheme` with the `light` color scheme.
+    /// It iterates through all `NamedSize` cases, rendering each illustration in a `UIHostingController`
+    /// and captures a snapshot. The snapshot is saved with a name indicating the elevation, theme, and color scheme.
+    @MainActor func testAllSizesInverseThemeLight() {
+        let theme = InverseTheme()
+        let interfaceStyle = UIUserInterfaceStyle.light
+        testSizeIconDecorative(for: theme, in: interfaceStyle)
+        testSizeIconWithTypography(for: theme, in: interfaceStyle)
+    }
+
+    // MARK: - Inverse Theme Dark Mode Size Tests
 
     /// This function tests all size tokens in the `InverseTheme` with the `dark` color scheme.
     /// It iterates through all `NamedSize` cases, rendering each illustration in a `UIHostingController`
-    /// and captures a snapshot. The snapshot is saved with a name indicating the size, theme, and color scheme.
-    @MainActor func testAllSizeInverseThemeDark() {
-        // IconDecorative
-        for sizing in NamedSize.IconDecorative.allCases {
-            // Use the `IllustrationIconDecorative` struct to test a single illustration
-            let illustration = OUDSThemeableView(theme: inverseTheme) {
-                SizeTokenPage.IllustrationIconDecorative(namedSize: sizing)
-                    .background(self.inverseTheme.colorBgPrimary.color(for: self.darkScheme))
+    /// and captures a snapshot. The snapshot is saved with a name indicating the elevation, theme, and color scheme.
+    @MainActor func testAllSizesInverseThemeDark() {
+        let theme = InverseTheme()
+        let interfaceStyle = UIUserInterfaceStyle.dark
+        testSizeIconDecorative(for: theme, in: interfaceStyle)
+        testSizeIconWithTypography(for: theme, in: interfaceStyle)
+    }
+
+    // MARK: Private test functions for all icon decorative properties of size token
+
+    /// Tests all icon decorative properties by capturing their snapshots.
+    /// - Parameters:
+    ///   - theme: Theme used for rendering tokens (e.g., OrangeTheme or InverseTheme).
+    ///   - interfaceStyle: The user interface style (light or dark) for which to test the colors.
+    @MainActor private func testSizeIconDecorative(for theme: OUDSTheme, in interfaceStyle: UIUserInterfaceStyle) {
+
+        // Iterate through all named tokens
+        for namedToken in NamedSize.IconDecorative.allCases {
+            // Use the `IllustrationWidth` struct to test a single illustration
+            let illustration = OUDSThemeableView(theme: theme) {
+                SizeTokenPage.IllustrationIconDecorative(namedSize: namedToken)
+                    .background(theme.colorBgPrimary.color(for: interfaceStyle == .light ? .light : .dark))
             }
 
             // Encapsulate the element in a UIHostingController for snapshot testing
             let hostingVC = UIHostingController(rootView: illustration)
 
-            // Capture the snapshot of the illustration with the correct theme and color scheme
-            let snapshotName = "\(sizing.rawValue)"
-            assertSnapshot(of: hostingVC, as: .image(traits: UITraitCollection(userInterfaceStyle: .dark)), named: snapshotName)
+            // Create a unique snapshot name based on the current mode (light or dark) and the color's raw value
+            let testName = "test_\(theme.name)Theme_\(interfaceStyle == .light ? "Light" : "Dark")"
+            let name = namedToken.rawValue
+
+            // Capture the snapshot of the illustration with the correct user interface style and save it with the snapshot name
+            assertSnapshot(of: hostingVC, as: .image(traits: UITraitCollection(userInterfaceStyle: interfaceStyle)), named: name, testName: testName)
         }
+    }
 
-        // IconWithLabel
-        for sizing in NamedSize.IconWithTypography.allCases {
-            // Use the `IllustrationIconWithLabel` struct to test a single illustration
-            let illustration = OUDSThemeableView(theme: inverseTheme) {
-                SizeTokenPage.IllustrationIconWithLabel(namedSize: sizing)
-                    .background(self.inverseTheme.colorBgPrimary.color(for: self.darkScheme))
+    /// Tests all icon with typography properties by capturing their snapshots.
+    /// - Parameters:
+    ///   - theme: Theme used for rendering tokens (e.g., OrangeTheme or InverseTheme).
+    ///   - interfaceStyle: The user interface style (light or dark) for which to test the colors.
+    @MainActor private func testSizeIconWithTypography(for theme: OUDSTheme, in interfaceStyle: UIUserInterfaceStyle) {
+
+        // Iterate through all named tokens
+        for namedToken in NamedSize.IconWithTypography.allCases {
+            // Use the `IllustrationWidth` struct to test a single illustration
+            let illustration = OUDSThemeableView(theme: theme) {
+                SizeTokenPage.IllustrationIconWithLabel(namedSize: namedToken)
+                    .background(theme.colorBgPrimary.color(for: interfaceStyle == .light ? .light : .dark))
             }
 
             // Encapsulate the element in a UIHostingController for snapshot testing
             let hostingVC = UIHostingController(rootView: illustration)
 
-            // Capture the snapshot of the illustration with the correct theme and color scheme
-            let snapshotName = "\(sizing.rawValue)"
-            assertSnapshot(of: hostingVC, as: .image(traits: UITraitCollection(userInterfaceStyle: .dark)), named: snapshotName)
+            // Create a unique snapshot name based on the current mode (light or dark) and the color's raw value
+            let testName = "test_\(theme.name)Theme_\(interfaceStyle == .light ? "Light" : "Dark")"
+            let name = namedToken.rawValue
+
+            // Capture the snapshot of the illustration with the correct user interface style and save it with the snapshot name
+            assertSnapshot(of: hostingVC, as: .image(traits: UITraitCollection(userInterfaceStyle: interfaceStyle)), named: name, testName: testName)
         }
     }
 }
