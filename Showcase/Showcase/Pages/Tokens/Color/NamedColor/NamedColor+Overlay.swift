cedcfad5914e5737974e0c141fb1e1ab041073f8
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2024-12-09 16:51:37 +0100

    refactor: update named colors for color oberlay
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

---
+++ Showcase/Showcase/Pages/Tokens/Color/NamedColor/NamedColor+Overlay.swift
@@ -0,0 +1,38 @@
+//
+// Software Name: OUDS iOS
+// SPDX-FileCopyrightText: Copyright (c) Orange SA
+// SPDX-License-Identifier: MIT
+// 
+// This software is distributed under the MIT license,
+// the text of which is available at https://opensource.org/license/MIT/
+// or see the "LICENSE" file for more details.
+// 
+// Authors: See CONTRIBUTORS.txt
+// Software description: A SwiftUI components library with code examples for Orange Unified Design System 
+//
+
+import OUDS
+import OUDSTokensSemantic
+
+extension NamedColor {
+    
+    enum Overlay: String, CaseIterable {
+        case colorOverlayDefault
+        case colorOverlayDrag
+        case colorOverlayEmphasized
+        case colorOverlayModal
+
+        func token(from theme: OUDSTheme) -> MultipleColorSemanticTokens {
+            switch self {
+            case .colorOverlayDefault:
+                return theme.colorOverlayDefault
+            case .colorOverlayDrag:
+                return theme.colorOverlayDrag
+            case .colorOverlayEmphasized:
+                return theme.colorOverlayEmphasized
+            case .colorOverlayModal:
+                return theme.colorOverlayModal
+            }
+        }
+    }
+}
