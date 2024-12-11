7ae3a82f0d30e74e080dc628eef2d49d75b3fa69
Ludovic PINEL <ludovic.pinel@orange.com>
2024-12-11 09:24:20 +0100

    Reafactoring to handle hover state

--- OUDS/Core/Components/Sources/Buttons/OUDSButtonStyle.swift
+++ OUDS/Core/Components/Sources/Buttons/OUDSButtonStyle.swift
@@ -15,6 +15,20 @@ import OUDS
 import OUDSTokensSemantic
 import SwiftUI
 
+/// Used to apply the right style on a `OUDSButton` according to the `hierarchy`
+/// and the `style`.
+///
+/// Four hierarchies are proposed:
+/// - default:
+/// - strong:
+/// - minimal:
+/// - negative:
+///
+/// Three styles are proposed:
+/// - normal: is the normal state of a button which can be disabled, focused, hovered or normal enabled
+/// - skeleton: the style used when screen if presenting and probably loading data.
+/// - loading: the style used after button was clicked and probably data are requested before navigate to a next screen or get updated data.
+///
 public struct OUDSButtonStyle: ButtonStyle {
 
     @Environment(\.theme) private var theme
@@ -27,46 +41,202 @@ public struct OUDSButtonStyle: ButtonStyle {
     public enum Hierarchy {
         case `default`, strong, minimal, negative
     }
+    public enum Style {
+        case normal, loading, skeleton
+    }
 
+    let style: Style
     let hierarchy: Hierarchy
+
+    @State var isHover: Bool
 
     // MARK: Initializer
 
-    public init(hierarchy: Hierarchy) {
+    public init(hierarchy: Hierarchy, style: Style) {
         self.hierarchy = hierarchy
+        self.style = style
+        self.isHover = false
     }
 
+    // MARK: Body
+
     public func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .modifier(ButtonBorderModifier(hierarchy: hierarchy, configuration: configuration))
-            .modifier(FouregroundModifier(hierarchy: hierarchy, configuration: configuration))
-            .modifier(BackgroundModifier(hierarchy: hierarchy, configuration: configuration))
+        switch style {
+        case .normal:
+            configuration.label
+                .onHover { isHover in
+                    self.isHover = isHover
+                }
+                .modifier(GeneralViewModifier(hierarchy: hierarchy, isEnabled: isEnable, isHover: isHover, isPressed: configuration.isPressed))
+        case .loading:
+            configuration.label
+                .modifier(ButtonLoadingModifier(hierarchy: hierarchy))
+                .modifier(GeneralViewModifier(hierarchy: hierarchy, isEnabled: true, isHover: false, isPressed: false))
+        case .skeleton:
+            configuration.label.modifier(ButtonSkeletonModifier())
+        }
     }
 }
 
-private struct FouregroundModifier: ViewModifier {
+enum ButtonState {
+    case enabled, hover, pressed, disabled, loading
+}
 
-    @Environment(\.theme) private var theme
-    @Environment(\.isEnabled) private var isEnable
-    @Environment(\.colorScheme) private var colorScheme
+// MARK: - General modifier
+
+///  This modifier has in charge to apply foreground, background colors
+/// and add a border (width, radius and color) associated to the hierarchy
+/// and according to the state (computed with `isEnabled`, `isPreessed` and `isHover` flags)
+private struct GeneralViewModifier: ViewModifier {
 
     let hierarchy: OUDSButtonStyle.Hierarchy
-    let configuration: ButtonStyleConfiguration
+    let isEnabled: Bool
+    let isHover: Bool
+    let isPressed: Bool
+
+    func body(content: Content) -> some View {
+        content
+            .modifier(ButtonBorderModifier(hierarchy: hierarchy, state: state))
+            .modifier(ButtonForegroundModifier(hierarchy: hierarchy, state: state))
+            .modifier(ButtonBackgroundModifier(hierarchy: hierarchy, state: state))
+    }
+
+    private var state: ButtonState {
+        if !isEnabled {
+            return .disabled
+        }
+
+        if isPressed {
+            return .pressed
+        }
+
+        if isHover {
+            return .hover
+        }
+
+        return .enabled
+    }
+}
+
+// MARK: - Skeleton Modifier
+
+/// Only used to apply the skeleton styl (grey rectangle over all)
+private struct ButtonSkeletonModifier: ViewModifier {
+
+    @Environment(\.theme) private var theme
+    @Environment(\.colorScheme) private var colorScheme
+
+    func body(content: Content) -> some View {
+        content.overlay(theme.colorGradientSkeletonMiddle.color(for: colorScheme)) // TODO: 💠_indicator/skeleton/color/bg
+    }
+}
+
+// MARK: - Loading modifiers
+
+private struct LoagingIndicator: View {
+
+    @State private var isAnimating = false
+    let color: Color
+
+    var body: some View {
+        Circle()
+            .trim(from: 0, to: 0.7)
+            .stroke(color, lineWidth: 3)
+            .rotationEffect(.degrees(isAnimating ? 360 : 0))
+            .onAppear {
+                withAnimation(Animation.linear(duration: 1).repeatForever(autoreverses: false)) {
+                    self.isAnimating.toggle()
+                }
+            }
+    }
+}
+
+/// Used to add a Progress indicator instead of conent (Text, Icon)
+/// As the button must keepp the size of the content, the indicator is
+/// added as overlay on top, and the content is hidden applying an opacity.
+private struct ButtonLoadingModifier: ViewModifier {
+
+    @Environment(\.theme) private var theme
+    @Environment(\.colorScheme) private var colorScheme
+
+    // MARK: Stored Properties
+
+    let hierarchy: OUDSButtonStyle.Hierarchy
+
+    // MARK: Body
+
+    func body(content: Content) -> some View {
+        content
+            .opacity(0)
+            .overlay {
+                LoagingIndicator(color: color)
+                    .padding(.vertical, theme.buttonSpacePaddingBlock)
+            }
+    }
+
+    // MARK: Private helper
+
+    private var color: Color {
+        switch hierarchy {
+        case .default:
+            theme.buttonColorContentDefaultLoading.color(for: colorScheme)
+        case .strong:
+            theme.colorActionPrimaryLoading.color(for: colorScheme) // TODO: colorActionLoading
+        case .minimal:
+            theme.buttonColorContentMinimalLoading.color(for: colorScheme)
+        case .negative:
+            theme.colorActionNegativeLoading.color(for: colorScheme)
+        }
+    }
+}
+
+// MARK: - Modifiers used by General Modifier
+
+// MARK: Foreground modifier
+
+/// Used to apply the right forgeround color associated to the hierarchy
+private struct ButtonForegroundModifier: ViewModifier {
+
+    @Environment(\.theme) private var theme
+    @Environment(\.colorScheme) private var colorScheme
+
+    // MARK: Stored Properties
+
+    let hierarchy: OUDSButtonStyle.Hierarchy
+    let state: ButtonState
+
+    // MARK: Body
 
     func body(content: Content) -> some View {
         content.foregroundStyle(colorToken.color(for: colorScheme))
     }
 
+    // MARK: Private helpers
+
     private var colorToken: MultipleColorSemanticTokens {
-        if configuration.isPressed {
-            return pressedToken
-        }
-
-        if isEnable {
+        switch state {
+        case .enabled, .loading:
             return token
+        case .hover:
+            return hoverToken
+        case .pressed:
+            return pressedToken
+        case .disabled:
+            return disabledToken
         }
+    }
 
-        return disabledToken
+    private var hoverToken: MultipleColorSemanticTokens {
+        switch hierarchy {
+        case .default:
+            theme.buttonColorContentDefaultHover
+        case .strong:
+            theme.colorActionPrimaryHover // colorActionHover
+        case .minimal:
+            theme.buttonColorContentMinimalHover
+        case .negative:
+            theme.colorActionNegativeHover
+        }
     }
 
     private var pressedToken: MultipleColorSemanticTokens {
@@ -108,29 +278,38 @@ private struct FouregroundModifier: ViewModifier {
     }
 }
 
-private struct BackgroundModifier: ViewModifier {
+// MARK: Border Modifier
+
+/// Used to apply the right background color associated to the hierarchy
+private struct ButtonBackgroundModifier: ViewModifier {
 
     @Environment(\.theme) private var theme
-    @Environment(\.isEnabled) private var isEnable
     @Environment(\.colorScheme) private var colorScheme
 
+    // MARK: Stored Properties
+
     let hierarchy: OUDSButtonStyle.Hierarchy
-    let configuration: ButtonStyleConfiguration
+    let state: ButtonState
+
+    // MARK: Body
 
     func body(content: Content) -> some View {
         content.background(colorToken.color(for: colorScheme))
     }
 
+    // MARK: Private helpers
+
     private var colorToken: MultipleColorSemanticTokens {
-        if configuration.isPressed {
-            return pressedToken
-        }
-
-        if isEnable {
+        switch state {
+        case .enabled, .loading:
             return token
+        case .hover:
+            return hoverToken
+        case .pressed:
+            return pressedToken
+        case .disabled:
+            return disbledToken
         }
-
-        return disbledToken
     }
 
     private var pressedToken: MultipleColorSemanticTokens {
@@ -171,15 +350,34 @@ private struct BackgroundModifier: ViewModifier {
             theme.colorActionDisabled
         }
     }
+
+    private var hoverToken: MultipleColorSemanticTokens {
+        switch hierarchy {
+        case .default:
+            theme.buttonColorBgDefaultHover
+        case .strong:
+            theme.colorContentOnActionPrimaryHover // theme.colorContentOnActionHover
+        case .minimal:
+            theme.buttonColorBgMinimalHover
+        case .negative:
+            theme.colorContentOnActionNegative
+        }
+    }
 }
 
+// MARK: Border modifier
+
+/// Used to apply a border with color, width and radius associated to the hierarchy
 private struct ButtonBorderModifier: ViewModifier {
 
     @Environment(\.theme) private var theme
-    @Environment(\.isEnabled) private var isEnable
+
+    // MARK: Stored Properties
 
     let hierarchy: OUDSButtonStyle.Hierarchy
-    let configuration: ButtonStyleConfiguration
+    let state: ButtonState
+
+    // MARK: Body
 
     func body(content: Content) -> some View {
         switch hierarchy {
@@ -188,7 +386,7 @@ private struct ButtonBorderModifier: ViewModifier {
                 .oudsBorder(
                     style: theme.borderStyleDefault,
                     width: theme.borderWidthDefault,
-                    radius: theme.borderRadiusTall, // buttonBorderRadius,
+                    radius: theme.buttonBorderRadius,
                     color: defaultColor)
         case .strong:
             content
@@ -204,37 +402,67 @@ private struct ButtonBorderModifier: ViewModifier {
         }
     }
 
-    // MARK: Default
-    private var defaultWidth: BorderWidthSemanticToken {
-        if configuration.isPressed {
-             return theme.buttonBorderWidthDefaultInteraction
-         } else {
-             return isEnable ? theme.buttonBorderWidthDefault : theme.buttonBorderWidthDefault
-         }
-     }
+    // MARK: Private helpers
 
-    private var defaultColor: MultipleColorSemanticTokens {
-        if configuration.isPressed {
-            return theme.buttonColorBorderDefaultPressed
-        } else {
-            return isEnable ? theme.buttonColorBorderDefaultEnabled : theme.buttonColorBorderDefaultDisabled
+    // MARK: Default hierarchy
+    private var defaultWidth: BorderWidthSemanticToken {
+        switch state {
+        case .enabled:
+            return theme.buttonBorderWidthDefault
+        case .hover:
+            return theme.buttonBorderWidthDefaultInteraction
+        case .pressed:
+            return theme.buttonBorderWidthDefaultInteraction
+        case .disabled:
+            return theme.buttonBorderWidthDefault
+        case .loading:
+            return theme.buttonBorderWidthDefaultInteraction
         }
     }
 
-    // MARK: Minimal
+    private var defaultColor: MultipleColorSemanticTokens {
+        switch state {
+        case .enabled:
+            return theme.buttonColorBorderDefaultEnabled
+        case .hover:
+            return theme.buttonColorBorderDefaultHover
+        case .pressed:
+            return theme.buttonColorBorderDefaultPressed
+        case .disabled:
+            return theme.buttonColorBorderDefaultDisabled
+        case .loading:
+            return theme.buttonColorBorderDefaultLoading
+        }
+    }
+
+    // MARK: Minimal hierarchy
     private var minimalWidth: BorderWidthSemanticToken {
-        if configuration.isPressed {
+        switch state {
+        case .enabled:
             return theme.buttonBorderWidthMinimal
-        } else {
-            return isEnable ? theme.buttonBorderWidthMinimalInteraction : theme.buttonBorderWidthMinimalInteraction
+        case .hover:
+            return theme.buttonBorderWidthMinimalInteraction
+        case .pressed:
+            return theme.buttonBorderWidthMinimalInteraction
+        case .disabled:
+            return theme.buttonBorderWidthMinimalInteraction
+        case .loading:
+            return theme.buttonBorderWidthMinimalInteraction
         }
     }
 
     private var minimalColor: MultipleColorSemanticTokens {
-        if configuration.isPressed {
-             return theme.buttonColorBorderMinimalPressed
-         } else {
-             return isEnable ? theme.buttonColorBorderMinimalEnabled : theme.buttonColorBorderMinimalDisabled
-         }
-     }
+        switch state {
+        case .enabled:
+            return theme.buttonColorBorderMinimalEnabled
+        case .hover:
+            return theme.buttonColorBorderMinimalHover
+        case .pressed:
+            return theme.buttonColorBorderMinimalPressed
+        case .disabled:
+            return theme.buttonColorBorderMinimalDisabled
+        case .loading:
+            return theme.buttonColorBorderMinimalLoading
+        }
+    }
 }
