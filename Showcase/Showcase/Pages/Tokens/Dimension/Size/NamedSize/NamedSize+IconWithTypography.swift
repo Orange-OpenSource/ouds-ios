a7fa113b1b7a1ccf2b1a3db8b9a47c33c45eae8c
LudovicPinel <ludovic.pinel@orange.com>
2025-01-17 11:25:21 +0100

    refactor: update size tokens screen of demo app (#284) (#396)
    Reviewed-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- Showcase/Showcase/Pages/Tokens/Dimension/Size/NamedSize/NamedSize+IconWithTypography.swift
+++ Showcase/Showcase/Pages/Tokens/Dimension/Size/NamedSize/NamedSize+IconWithTypography.swift
@@ -16,176 +16,113 @@ import OUDSTokensSemantic
 import SwiftUICore
 
 extension NamedSize {
-
     enum IconWithTypography: String, CaseIterable {
-        case sizeIconWithHeadingXlargeShort
-        case sizeIconWithHeadingXlargeMedium
-        case sizeIconWithHeadingXlargeTall
-        case sizeIconWithHeadingLargeShort
-        case sizeIconWithHeadingLargeMedium
-        case sizeIconWithHeadingLargeTall
-        case sizeIconWithHeadingMediumShort
-        case sizeIconWithHeadingMediumMedium
-        case sizeIconWithHeadingMediumTall
-        case sizeIconWithHeadingSmallShort
-        case sizeIconWithHeadingSmallMedium
-        case sizeIconWithHeadingSmallTall
-        case sizeIconWithBodyLargeShort
-        case sizeIconWithBodyLargeMedium
-        case sizeIconWithBodyLargeTall
-        case sizeIconWithBodyMediumShort
-        case sizeIconWithBodyMediumMedium
-        case sizeIconWithBodyMediumTall
-        case sizeIconWithBodySmallShort
-        case sizeIconWithBodySmallMedium
-        case sizeIconWithBodySmallTall
+
+        // MARK: With Heading
+        case sizeIconWithHeadingXlargeSizeSm
+        case sizeIconWithHeadingXlargeSizeMd
+        case sizeIconWithHeadingXlargeSizeLg
+
+        case sizeIconWithHeadingLargeSizeSm
+        case sizeIconWithHeadingLargeSizeMd
+        case sizeIconWithHeadingLargeSizeLg
+
+        case sizeIconWithHeadingMediumSizeSm
+        case sizeIconWithHeadingMediumSizeMd
+        case sizeIconWithHeadingMediumSizeLg
+
+        case sizeIconWithHeadingSmallSizeSm
+        case sizeIconWithHeadingSmallSizeMd
+        case sizeIconWithHeadingSmallSizeLg
+
+        // MARK: With Body
+        case sizeIconWithBodyLargeSizeSm
+        case sizeIconWithBodyLargeSizeMd
+        case sizeIconWithBodyLargeSizeLg
+
+        case sizeIconWithBodyMediumSizeSm
+        case sizeIconWithBodyMediumSizeMd
+        case sizeIconWithBodyMediumSizeLg
+
+        case sizeIconWithBodySmallSizeSm
+        case sizeIconWithBodySmallSizeMd
+        case sizeIconWithBodySmallSizeLg
+
+        // MARK: With Label
         case sizeIconWithLabelXlargeSizeSm
         case sizeIconWithLabelXlargeSizeMd
         case sizeIconWithLabelXlargeSizeLg
+
         case sizeIconWithLabelLargeSizeXs
         case sizeIconWithLabelLargeSizeSm
         case sizeIconWithLabelLargeSizeMd
         case sizeIconWithLabelLargeSizeLg
         case sizeIconWithLabelLargeSizeXl
+
         case sizeIconWithLabelMediumSizeXs
         case sizeIconWithLabelMediumSizeSm
         case sizeIconWithLabelMediumSizeMd
         case sizeIconWithLabelMediumSizeLg
+
         case sizeIconWithLabelSmallSizeXs
         case sizeIconWithLabelSmallSizeSm
         case sizeIconWithLabelSmallSizeMd
         case sizeIconWithLabelSmallSizeLg
 
-        var namedFont: NamedFont {
-            switch self {
-            case .sizeIconWithHeadingXlargeShort:
-                return .headingXLarge
-            case .sizeIconWithHeadingXlargeMedium:
-                return .headingXLarge
-            case .sizeIconWithHeadingXlargeTall:
-                return .headingXLarge
-            case .sizeIconWithHeadingLargeShort:
-                return .headingLarge
-            case .sizeIconWithHeadingLargeMedium:
-                return .headingLarge
-            case .sizeIconWithHeadingLargeTall:
-                return .headingLarge
-            case .sizeIconWithHeadingMediumShort:
-                return .headingMedium
-            case .sizeIconWithHeadingMediumMedium:
-                return .headingMedium
-            case .sizeIconWithHeadingMediumTall:
-                return .headingMedium
-            case .sizeIconWithHeadingSmallShort:
-                return .headingSmall
-            case .sizeIconWithHeadingSmallMedium:
-                return .headingSmall
-            case .sizeIconWithHeadingSmallTall:
-                return .headingSmall
-            case .sizeIconWithBodyLargeShort:
-                return .bodyDefaultLarge
-            case .sizeIconWithBodyLargeMedium:
-                return .bodyDefaultLarge
-            case .sizeIconWithBodyLargeTall:
-                return .bodyDefaultLarge
-            case .sizeIconWithBodyMediumShort:
-                return .bodyDefaultMedium
-            case .sizeIconWithBodyMediumMedium:
-                return .bodyDefaultMedium
-            case .sizeIconWithBodyMediumTall:
-                return .bodyDefaultMedium
-            case .sizeIconWithBodySmallShort:
-                return .bodyDefaultSmall
-            case .sizeIconWithBodySmallMedium:
-                return .bodyDefaultSmall
-            case .sizeIconWithBodySmallTall:
-                return .bodyDefaultSmall
-            case .sizeIconWithLabelXlargeSizeSm:
-                return .labelDefaultXLarge
-            case .sizeIconWithLabelXlargeSizeMd:
-                return .labelDefaultXLarge
-            case .sizeIconWithLabelXlargeSizeLg:
-                return .labelDefaultXLarge
-            case .sizeIconWithLabelLargeSizeXs:
-                return .labelDefaultXLarge
-            case .sizeIconWithLabelLargeSizeSm:
-                return .labelDefaultLarge
-            case .sizeIconWithLabelLargeSizeMd:
-                return .labelDefaultLarge
-            case .sizeIconWithLabelLargeSizeLg:
-                return .labelDefaultLarge
-            case .sizeIconWithLabelLargeSizeXl:
-                return .labelDefaultLarge
-            case .sizeIconWithLabelMediumSizeXs:
-                return .labelDefaultMedium
-            case .sizeIconWithLabelMediumSizeSm:
-                return .labelDefaultMedium
-            case .sizeIconWithLabelMediumSizeMd:
-                return .labelDefaultMedium
-            case .sizeIconWithLabelMediumSizeLg:
-                return .labelDefaultMedium
-            case .sizeIconWithLabelSmallSizeXs:
-                return .labelDefaultSmall
-            case .sizeIconWithLabelSmallSizeSm:
-                return .labelDefaultSmall
-            case .sizeIconWithLabelSmallSizeMd:
-                return .labelDefaultSmall
-            case .sizeIconWithLabelSmallSizeLg:
-                return .labelDefaultSmall
-            }
-        }
-
         // swiftlint:disable function_body_length
-        func token(fot theme: OUDSTheme, userInterfaceSizeClass: UserInterfaceSizeClass) -> SizeSemanticToken {
+        func token(for theme: OUDS.OUDSTheme, userInterfaceSizeClass: UserInterfaceSizeClass) -> OUDSTokensSemantic.SizeSemanticToken {
             switch self {
-            case .sizeIconWithHeadingXlargeShort:
+                // Heading
+            case .sizeIconWithHeadingXlargeSizeSm:
                 return theme.sizes.sizeIconWithHeadingXlargeSizeSm.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithHeadingXlargeMedium:
+            case .sizeIconWithHeadingXlargeSizeMd:
                 return theme.sizes.sizeIconWithHeadingXlargeSizeMd.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithHeadingXlargeTall:
+            case .sizeIconWithHeadingXlargeSizeLg:
                 return theme.sizes.sizeIconWithHeadingXlargeSizeLg.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithHeadingLargeShort:
+            case .sizeIconWithHeadingLargeSizeSm:
                 return theme.sizes.sizeIconWithHeadingLargeSizeSm.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithHeadingLargeMedium:
+            case .sizeIconWithHeadingLargeSizeMd:
                 return theme.sizes.sizeIconWithHeadingLargeSizeMd.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithHeadingLargeTall:
+            case .sizeIconWithHeadingLargeSizeLg:
                 return theme.sizes.sizeIconWithHeadingLargeSizeLg.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithHeadingMediumShort:
+            case .sizeIconWithHeadingMediumSizeSm:
                 return theme.sizes.sizeIconWithHeadingMediumSizeSm.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithHeadingMediumMedium:
+            case .sizeIconWithHeadingMediumSizeMd:
                 return theme.sizes.sizeIconWithHeadingMediumSizeMd.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithHeadingMediumTall:
+            case .sizeIconWithHeadingMediumSizeLg:
                 return theme.sizes.sizeIconWithHeadingMediumSizeLg.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithHeadingSmallShort:
+            case .sizeIconWithHeadingSmallSizeSm:
                 return theme.sizes.sizeIconWithHeadingSmallSizeSm.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithHeadingSmallMedium:
+            case .sizeIconWithHeadingSmallSizeMd:
                 return theme.sizes.sizeIconWithHeadingSmallSizeMd.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithHeadingSmallTall:
+            case .sizeIconWithHeadingSmallSizeLg:
                 return theme.sizes.sizeIconWithHeadingSmallSizeLg.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithBodyLargeShort:
+            // Body
+            case .sizeIconWithBodyLargeSizeSm:
                 return theme.sizes.sizeIconWithBodyLargeSizeSm.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithBodyLargeMedium:
+            case .sizeIconWithBodyLargeSizeMd:
                 return theme.sizes.sizeIconWithBodyLargeSizeMd.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithBodyLargeTall:
+            case .sizeIconWithBodyLargeSizeLg:
                 return theme.sizes.sizeIconWithBodyLargeSizeLg.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithBodyMediumShort:
+            case .sizeIconWithBodyMediumSizeSm:
                 return theme.sizes.sizeIconWithBodyMediumSizeSm.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithBodyMediumMedium:
+            case .sizeIconWithBodyMediumSizeMd:
                 return theme.sizes.sizeIconWithBodyMediumSizeMd.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithBodyMediumTall:
+            case .sizeIconWithBodyMediumSizeLg:
                 return theme.sizes.sizeIconWithBodyMediumSizeLg.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithBodySmallShort:
+            case .sizeIconWithBodySmallSizeSm:
                 return theme.sizes.sizeIconWithBodySmallSizeSm.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithBodySmallMedium:
+            case .sizeIconWithBodySmallSizeMd:
                 return theme.sizes.sizeIconWithBodySmallSizeMd.dimension(for: userInterfaceSizeClass)
-            case .sizeIconWithBodySmallTall:
+            case .sizeIconWithBodySmallSizeLg:
                 return theme.sizes.sizeIconWithBodySmallSizeLg.dimension(for: userInterfaceSizeClass)
+                // With Label
             case .sizeIconWithLabelXlargeSizeSm:
-                return theme.sizes.sizeIconWithLabelXlargeSizeSm
+                return theme.sizes.sizeIconWithLabelLargeSizeSm
             case .sizeIconWithLabelXlargeSizeMd:
-                return theme.sizes.sizeIconWithLabelXlargeSizeMd
+                return theme.sizes.sizeIconWithLabelLargeSizeMd
             case .sizeIconWithLabelXlargeSizeLg:
-                return theme.sizes.sizeIconWithLabelXlargeSizeLg
+                return theme.sizes.sizeIconWithLabelLargeSizeLg
             case .sizeIconWithLabelLargeSizeXs:
                 return theme.sizes.sizeIconWithLabelLargeSizeXs
             case .sizeIconWithLabelLargeSizeSm:
@@ -215,5 +152,130 @@ extension NamedSize {
             }
         }
         // swiftlint:enable function_body_length
+
+        /// Provides the `namedFont associated to the icon size
+        var namedFont: NamedFont {
+            switch self {
+            case .sizeIconWithHeadingXlargeSizeSm,
+                    .sizeIconWithHeadingXlargeSizeMd,
+                    .sizeIconWithHeadingXlargeSizeLg:
+                return .headingXLarge
+            case .sizeIconWithHeadingLargeSizeSm,
+                    .sizeIconWithHeadingLargeSizeMd,
+                    .sizeIconWithHeadingLargeSizeLg:
+                return .headingLarge
+            case .sizeIconWithHeadingMediumSizeSm,
+                    .sizeIconWithHeadingMediumSizeMd,
+                    .sizeIconWithHeadingMediumSizeLg:
+                return .headingMedium
+            case .sizeIconWithHeadingSmallSizeSm,
+                    .sizeIconWithHeadingSmallSizeMd,
+                    .sizeIconWithHeadingSmallSizeLg:
+                return .headingSmall
+            case .sizeIconWithBodyLargeSizeSm,
+                    .sizeIconWithBodyLargeSizeMd,
+                    .sizeIconWithBodyLargeSizeLg:
+                return .bodyDefaultLarge
+            case .sizeIconWithBodyMediumSizeSm,
+                    .sizeIconWithBodyMediumSizeMd,
+                    .sizeIconWithBodyMediumSizeLg:
+                return .bodyDefaultMedium
+            case .sizeIconWithBodySmallSizeSm,
+                    .sizeIconWithBodySmallSizeMd,
+                    .sizeIconWithBodySmallSizeLg:
+                return .bodyDefaultSmall
+            case .sizeIconWithLabelXlargeSizeSm,
+                    .sizeIconWithLabelXlargeSizeMd,
+                    .sizeIconWithLabelXlargeSizeLg:
+                return .labelDefaultXLarge
+            case .sizeIconWithLabelLargeSizeXs,
+                    .sizeIconWithLabelLargeSizeSm,
+                    .sizeIconWithLabelLargeSizeMd,
+                    .sizeIconWithLabelLargeSizeLg,
+                    .sizeIconWithLabelLargeSizeXl:
+                return .labelDefaultLarge
+            case .sizeIconWithLabelMediumSizeXs,
+                    .sizeIconWithLabelMediumSizeSm,
+                    .sizeIconWithLabelMediumSizeMd,
+                    .sizeIconWithLabelMediumSizeLg:
+                return .labelDefaultMedium
+            case .sizeIconWithLabelSmallSizeXs,
+                    .sizeIconWithLabelSmallSizeSm,
+                    .sizeIconWithLabelSmallSizeMd,
+                    .sizeIconWithLabelSmallSizeLg:
+                return .labelDefaultSmall
+            }
+        }
+
+        var sizeDescription: String {
+            switch self {
+            case .sizeIconWithHeadingXlargeSizeSm,
+                    .sizeIconWithHeadingLargeSizeSm,
+                    .sizeIconWithHeadingMediumSizeSm,
+                    .sizeIconWithHeadingSmallSizeSm,
+                    .sizeIconWithBodyLargeSizeSm,
+                    .sizeIconWithBodyMediumSizeSm,
+                    .sizeIconWithBodySmallSizeSm,
+                    .sizeIconWithLabelXlargeSizeSm,
+                    .sizeIconWithLabelLargeSizeSm,
+                    .sizeIconWithLabelMediumSizeSm,
+                    .sizeIconWithLabelSmallSizeSm:
+                "Size Samll"
+            case .sizeIconWithHeadingXlargeSizeMd,
+                    .sizeIconWithHeadingLargeSizeMd,
+                    .sizeIconWithHeadingMediumSizeMd,
+                    .sizeIconWithHeadingSmallSizeMd,
+                    .sizeIconWithBodyLargeSizeMd,
+                    .sizeIconWithBodyMediumSizeMd,
+                    .sizeIconWithBodySmallSizeMd,
+                    .sizeIconWithLabelXlargeSizeMd,
+                    .sizeIconWithLabelLargeSizeMd,
+                    .sizeIconWithLabelMediumSizeMd,
+                    .sizeIconWithLabelSmallSizeMd:
+                "Size Medium"
+            case .sizeIconWithHeadingXlargeSizeLg,
+                    .sizeIconWithHeadingLargeSizeLg,
+                    .sizeIconWithHeadingMediumSizeLg,
+                    .sizeIconWithHeadingSmallSizeLg,
+                    .sizeIconWithBodyLargeSizeLg,
+                    .sizeIconWithBodyMediumSizeLg,
+                    .sizeIconWithBodySmallSizeLg,
+                    .sizeIconWithLabelXlargeSizeLg,
+                    .sizeIconWithLabelLargeSizeLg,
+                    .sizeIconWithLabelMediumSizeLg,
+                    .sizeIconWithLabelSmallSizeLg:
+                "Size Large"
+            case .sizeIconWithLabelSmallSizeXs,
+                    .sizeIconWithLabelMediumSizeXs,
+                    .sizeIconWithLabelLargeSizeXs:
+                "Extra Small"
+            case .sizeIconWithLabelLargeSizeXl:
+                "Extra Large"
+            }
+        }
+    }
+
+    /// Used to illustrate icon sizes grouped by
+    /// typography category
+    struct SizeIconByTypographyCategory {
+        let namedFont: NamedFont
+        var sizes: [NamedSize.IconWithTypography]
+    }
+
+    /// Provides all sizes grouped by typpgraphy categories
+    static var iconSizeByTypographyCategories: [NamedSize.SizeIconByTypographyCategory] {
+        var lastCategory: NamedSize.SizeIconByTypographyCategory?
+
+        return NamedSize.IconWithTypography.allCases.reduce(into: []) { partialResult, namedSize in
+            if namedSize.namedFont == lastCategory?.namedFont {
+                lastCategory?.sizes.append(namedSize)
+            } else {
+                if let category = lastCategory {
+                    partialResult.append(category)
+                }
+
+                lastCategory = SizeIconByTypographyCategory(namedFont: namedSize.namedFont, sizes: [namedSize])
+            }
+        }
     }
 }
--- Showcase/Showcase/Pages/Tokens/Dimension/Size/SizeTokenPage.swift
+++ Showcase/Showcase/Pages/Tokens/Dimension/Size/SizeTokenPage.swift
@@ -15,6 +15,8 @@ import OUDS
 import OUDSTokensSemantic
 import SwiftUI
 
+// MARK: - Size Token Page
+
 struct SizeTokenPage: View {
 
     @Environment(\.theme) private var theme
@@ -31,18 +33,23 @@ struct SizeTokenPage: View {
                     }
                 }
             } header: {
-                header("app_tokens_dimension_size_iconDecorative_label")
+                sectionHeader("app_tokens_dimension_size_iconDecorative_label")
             }
+
             Section {
-                ForEach(NamedSize.IconWithTypography.allCases, id: \.rawValue) { namedIconSize in
-                    IllustrationIconWithLabel(namedSize: namedIconSize)
+                VStack(alignment: .leading, spacing: theme.spaces.spaceFixedMedium) {
+                    ForEach(NamedSize.iconSizeByTypographyCategories, id: \.namedFont) { category in
+                        IllustrationSizeIconByTypographyCategory(category: category)
+                    }
                 }
             } header: {
-                header("app_tokens_dimension_size_iconWithLabel_label")
+                sectionHeader("app_tokens_dimension_size_iconWith_label")
             }
         }
         .padding(.horizontal, theme.spaces.spaceFixedMedium)
     }
+
+    // MARK: Illustration Icon Decorative
 
     struct IllustrationIconDecorative: View {
         @Environment(\.theme) private var theme
@@ -72,11 +79,12 @@ struct SizeTokenPage: View {
         }
     }
 
-    private func header(_ text: LocalizedStringKey) -> some View {
-        Text(text).showcaseSectionHeaderStyle()
-    }
+    // MARK: Illustration Icon With Typography
 
-    struct IllustrationIconWithLabel: View {
+    struct IllustrationIconWithTypography: View {
+
+        // MARK: Stored properties
+
         @Environment(\.theme) private var theme
         @Environment(\.colorScheme) private var colorScheme
         @Environment(\.horizontalSizeClass) private var horizontalSizeClass
@@ -84,29 +92,89 @@ struct SizeTokenPage: View {
         let namedSize: NamedSize.IconWithTypography
 
         var body: some View {
-            let token = namedSize.token(fot: theme, userInterfaceSizeClass: horizontalSizeClass ?? .regular)
-            let namedFont = namedSize.namedFont
-            let value = String(format: "\(namedSize.rawValue) (%.0f pt)", token)
+            let token = namedSize.token(for: theme, userInterfaceSizeClass: horizontalSizeClass ?? .regular)
+            let name = namedSize.sizeDescription
+            let value = String(format: "%.2f pt", token)
 
-            HStack {
-                Image("ic_token")
-                    .resizable()
-                    .renderingMode(.template)
-                    .foregroundColor(theme.colors.colorContentStatusInfo.color(for: colorScheme))
-                    .frame(width: token, height: token, alignment: .center)
-                    .accessibilityHidden(true)
-
-                VStack(alignment: .leading) {
-                    illustration(for: namedFont, in: theme)
-                        .foregroundStyle(theme.colors.colorContentDefault.color(for: colorScheme))
-                    Text(value)
-                        .typeBodyDefaultMedium(theme)
-                        .foregroundStyle(theme.colors.colorContentMuted.color(for: colorScheme))
+            ShowcaseTokenIllustration(tokenName: name, tokenValue: value) {
+                ZStack {
+                    Rectangle()
+                        .fill(theme.colors.colorSurfaceStatusNeutralMuted.color(for: colorScheme))
+                        .frame(width: 82, height: 82, alignment: .center)
+                    Image("ic_token")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(theme.colors.colorContentStatusInfo.color(for: colorScheme))
+                        .frame(width: token, height: token, alignment: .center)
+                        .accessibilityHidden(true)
                 }
-                .accessibilityElement(children: .combine)
             }
         }
     }
+
+    // MARK: Illustration Size Icon By Typography Category
+
+    private struct IllustrationSizeIconByTypographyCategory: View {
+
+        @Environment(\.theme) private var theme
+        @Environment(\.colorScheme) private var colorScheme
+        @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+
+        let category: NamedSize.SizeIconByTypographyCategory
+
+        var body: some View {
+            if let lastSize = category.sizes.last {
+                TypographyCategoryHeader(namedFont: category.namedFont, namedSize: lastSize)
+            }
+
+            VStack(alignment: .leading, spacing: theme.spaces.spaceFixedMedium) {
+                ForEach(category.sizes, id: \.sizeDescription) { namedSize in
+                    IllustrationIconWithTypography(namedSize: namedSize)
+                }
+            }
+        }
+    }
+
+    // MARK: Typography Category Header
+
+    private struct TypographyCategoryHeader: View {
+
+        @Environment(\.theme) private var theme
+        @Environment(\.colorScheme) private var colorScheme
+        @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+
+        let namedFont: NamedFont
+        let namedSize: NamedSize.IconWithTypography
+
+        private var size: CGFloat {
+            namedSize.token(for: theme, userInterfaceSizeClass: self.horizontalSizeClass ?? .regular)
+        }
+
+        var body: some View {
+            HStack(alignment: .center, spacing: theme.spaces.spaceFixedShort) {
+                HStack(alignment: .center, spacing: 1) {
+                    Image(decorative: "ic_token")
+                        .resizable()
+                        .renderingMode(.template)
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundStyle(theme.colors.colorContentStatusInfo.color(for: colorScheme))
+                }
+                .frame(height: size, alignment: .center)
+
+                illustration(for: namedFont, in: theme)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundStyle(theme.colors.colorContentDefault.color(for: colorScheme))
+            }
+            .padding(.all, theme.spaces.spaceFixedMedium)
+            .background(theme.colors.colorSurfaceStatusNeutralMuted.color(for: colorScheme))
+        }
+    }
+
+    private func sectionHeader(_ text: LocalizedStringKey) -> some View {
+        Text(text).showcaseSectionHeaderStyle()
+    }
 }
+
+// MARK: - Named Size
 
 enum NamedSize { }
