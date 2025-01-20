1eb829e8ab94f0269d7c44e6fb7d18c633056cbc
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2025-01-20 10:44:07 +0100

    refactor: xcodeproj, xcworkspace, Podfile, targets, pods (#85)
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- DesignToolbox/Pods/Target Support Files/Pods-DesignToolbox/Pods-DesignToolbox-umbrella.h
+++ DesignToolbox/Pods/Target Support Files/Pods-DesignToolbox/Pods-DesignToolbox-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_ShowcaseVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ShowcaseVersionString[];
+FOUNDATION_EXPORT double Pods_DesignToolboxVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DesignToolboxVersionString[];
 
