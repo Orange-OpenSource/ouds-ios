45042fce1e39ac7684d589cc5ddbdd94547b90ac
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2025-02-19 16:56:54 +0100

    refactor: tests for elevation raw tokens (#340)
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- OUDS/Core/Tokens/RawTokens/Tests/ElevationRawTokensTests.swift
+++ OUDS/Core/Tokens/RawTokens/Tests/ElevationRawTokensTests.swift
@@ -16,9 +16,7 @@ import OUDSTokensRaw
 import SwiftUI
 import Testing
 import TestsUtils
-import XCTest
 
-// swiftlint:disable required_deinit
 // swiftlint:disable type_body_length
 // swiftlint:disable function_body_length
 // swiftlint:disable file_length
@@ -39,7 +37,7 @@ import XCTest
 /// - the "bigger" the Y token is, the higher the value is
 /// - all composites must be different
 /// - elevations in a same "category" must have darker and darker colors
-final class ElevationRawTokensTests: XCTestCase {
+struct ElevationRawTokensTests {
 
     // MARK: - Computed properties
 
@@ -253,223 +251,223 @@ final class ElevationRawTokensTests: XCTestCase {
 
     // MARK: - Primitive token - Elevation - Box Shadow
 
-    func testElevationBottom_1_100ColorDarkerThan_1_200() throws {
+    @Test func elevationBottom_1_100ColorDarkerThan_1_200() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_1_100.color, ElevationRawTokens.elevationBottom_1_200.color)
     }
 
-    func testElevationBottom_1_200ColorDarkerThan_1_300() throws {
+    @Test func elevationBottom_1_200ColorDarkerThan_1_300() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_1_200.color, ElevationRawTokens.elevationBottom_1_300.color)
     }
 
-    func testElevationBottom_1_300ColorDarkerThan_1_400() throws {
+    @Test func elevationBottom_1_300ColorDarkerThan_1_400() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_1_300.color, ElevationRawTokens.elevationBottom_1_400.color)
     }
 
-    func testElevationBottom_1_400ColorDarkerThan_1_500() throws {
+    @Test func elevationBottom_1_400ColorDarkerThan_1_500() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_1_400.color, ElevationRawTokens.elevationBottom_1_500.color)
     }
 
-    func testElevationBottom_1_500ColorDarkerThan_1_600() throws {
+    @Test func elevationBottom_1_500ColorDarkerThan_1_600() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_1_500.color, ElevationRawTokens.elevationBottom_1_600.color)
     }
 
-    func testElevationBottom_2_100ColorDarkerThan_2_200() throws {
+    @Test func elevationBottom_2_100ColorDarkerThan_2_200() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_2_100.color, ElevationRawTokens.elevationBottom_2_200.color)
     }
 
-    func testElevationBottom_2_200ColorDarkerThan_2_300() throws {
+    @Test func elevationBottom_2_200ColorDarkerThan_2_300() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_2_200.color, ElevationRawTokens.elevationBottom_2_300.color)
     }
 
-    func testElevationBottom_2_300ColorDarkerThan_2_400() throws {
+    @Test func elevationBottom_2_300ColorDarkerThan_2_400() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_2_300.color, ElevationRawTokens.elevationBottom_2_400.color)
     }
 
-    func testElevationBottom_2_400ColorDarkerThan_2_500() throws {
+    @Test func elevationBottom_2_400ColorDarkerThan_2_500() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_2_400.color, ElevationRawTokens.elevationBottom_2_500.color)
     }
 
-    func testElevationBottom_2_500ColorDarkerThan_2_600() throws {
+    @Test func elevationBottom_2_500ColorDarkerThan_2_600() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_2_500.color, ElevationRawTokens.elevationBottom_2_600.color)
     }
 
-    func testElevationBottom_3_100ColorDarkerThan_3_200() throws {
+    @Test func elevationBottom_3_100ColorDarkerThan_3_200() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_3_100.color, ElevationRawTokens.elevationBottom_3_200.color)
     }
 
-    func testElevationBottom_3_200ColorDarkerThan_3_300() throws {
+    @Test func elevationBottom_3_200ColorDarkerThan_3_300() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_3_200.color, ElevationRawTokens.elevationBottom_3_300.color)
     }
 
-    func testElevationBottom_3_300ColorDarkerThan_3_400() throws {
+    @Test func elevationBottom_3_300ColorDarkerThan_3_400() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_3_300.color, ElevationRawTokens.elevationBottom_3_400.color)
     }
 
-    func testElevationBottom_3_400ColorDarkerThan_3_500() throws {
+    @Test func elevationBottom_3_400ColorDarkerThan_3_500() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_3_400.color, ElevationRawTokens.elevationBottom_3_500.color)
     }
 
-    func testElevationBottom_3_500ColorDarkerThan_3_600() throws {
+    @Test func elevationBottom_3_500ColorDarkerThan_3_600() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_3_500.color, ElevationRawTokens.elevationBottom_3_600.color)
     }
 
-    func testElevationBottom_4_100ColorDarkerThan_4_200() throws {
+    @Test func elevationBottom_4_100ColorDarkerThan_4_200() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_4_100.color, ElevationRawTokens.elevationBottom_4_200.color)
     }
 
-    func testElevationBottom_4_200ColorDarkerThan_4_300() throws {
+    @Test func elevationBottom_4_200ColorDarkerThan_4_300() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_4_200.color, ElevationRawTokens.elevationBottom_4_300.color)
     }
 
-    func testElevationBottom_4_300ColorDarkerThan_4_400() throws {
+    @Test func elevationBottom_4_300ColorDarkerThan_4_400() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_4_300.color, ElevationRawTokens.elevationBottom_4_400.color)
     }
 
-    func testElevationBottom_4_400ColorDarkerThan_4_500() throws {
+    @Test func elevationBottom_4_400ColorDarkerThan_4_500() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_4_400.color, ElevationRawTokens.elevationBottom_4_500.color)
     }
 
-    func testElevationBottom_4_500ColorDarkerThan_4_600() throws {
+    @Test func elevationBottom_4_500ColorDarkerThan_4_600() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_4_500.color, ElevationRawTokens.elevationBottom_4_600.color)
     }
 
-    func testElevationBottom_5_100ColorDarkerThan_5_200() throws {
+    @Test func elevationBottom_5_100ColorDarkerThan_5_200() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_5_100.color, ElevationRawTokens.elevationBottom_5_200.color)
     }
 
-    func testElevationBottom_5_200ColorDarkerThan_5_300() throws {
+    @Test func elevationBottom_5_200ColorDarkerThan_5_300() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_5_200.color, ElevationRawTokens.elevationBottom_5_300.color)
     }
 
-    func testElevationBottom_5_300ColorDarkerThan_5_400() throws {
+    @Test func elevationBottom_5_300ColorDarkerThan_5_400() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_5_300.color, ElevationRawTokens.elevationBottom_5_400.color)
     }
 
-    func testElevationBottom_5_400ColorDarkerThan_5_500() throws {
+    @Test func elevationBottom_5_400ColorDarkerThan_5_500() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_5_400.color, ElevationRawTokens.elevationBottom_5_500.color)
     }
 
-    func testElevationBottom_5_500ColorDarkerThan_5_600() throws {
+    @Test func elevationBottom_5_500ColorDarkerThan_5_600() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_5_500.color, ElevationRawTokens.elevationBottom_5_600.color)
     }
 
-    func testElevationBottom_5_100ColorDarkerThan_6_200() throws {
+    @Test func elevationBottom_5_100ColorDarkerThan_6_200() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_5_100.color, ElevationRawTokens.elevationBottom_6_200.color)
     }
 
-    func testElevationBottom_5_200ColorDarkerThan_6_300() throws {
+    @Test func elevationBottom_5_200ColorDarkerThan_6_300() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_5_200.color, ElevationRawTokens.elevationBottom_6_300.color)
     }
 
-    func testElevationBottom_5_300ColorDarkerThan_6_400() throws {
+    @Test func elevationBottom_5_300ColorDarkerThan_6_400() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_5_300.color, ElevationRawTokens.elevationBottom_6_400.color)
     }
 
-    func testElevationBottom_5_400ColorDarkerThan_6_500() throws {
+    @Test func elevationBottom_5_400ColorDarkerThan_6_500() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_5_400.color, ElevationRawTokens.elevationBottom_6_500.color)
     }
 
-    func testElevationBottom_5_500ColorDarkerThan_6_600() throws {
+    @Test func elevationBottom_5_500ColorDarkerThan_6_600() throws {
         TestsUtils.assertColorDarkerThan(ElevationRawTokens.elevationBottom_5_500.color, ElevationRawTokens.elevationBottom_6_600.color)
     }
 
-    func testCompositeBottom_1_100_LowerThan_2_100() throws {
+    @Test func compositeBottom_1_100_LowerThan_2_100() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_1_100, ElevationRawTokens.elevationBottom_2_100)
     }
 
-    func testCompositeBottom_2_100_LowerThan_3_100() throws {
+    @Test func compositeBottom_2_100_LowerThan_3_100() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_2_100, ElevationRawTokens.elevationBottom_3_100)
     }
 
-    func testCompositeBottom_3_100_LowerThan_4_100() throws {
+    @Test func compositeBottom_3_100_LowerThan_4_100() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_3_100, ElevationRawTokens.elevationBottom_4_100)
     }
 
-    func testCompositeBottom_4_100_LowerThan_5_100() throws {
+    @Test func compositeBottom_4_100_LowerThan_5_100() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_4_100, ElevationRawTokens.elevationBottom_5_100)
     }
 
-    func testCompositeBottom_5_100_LowerThan_6_100() throws {
+    @Test func compositeBottom_5_100_LowerThan_6_100() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_5_100, ElevationRawTokens.elevationBottom_6_100)
     }
 
-    func testCompositeBottom_1_200_LowerThan_2_200() throws {
+    @Test func compositeBottom_1_200_LowerThan_2_200() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_1_200, ElevationRawTokens.elevationBottom_2_200)
     }
 
-    func testCompositeBottom_2_200_LowerThan_3_200() throws {
+    @Test func compositeBottom_2_200_LowerThan_3_200() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_2_200, ElevationRawTokens.elevationBottom_3_200)
     }
 
-    func testCompositeBottom_3_200_LowerThan_4_200() throws {
+    @Test func compositeBottom_3_200_LowerThan_4_200() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_3_200, ElevationRawTokens.elevationBottom_4_200)
     }
 
-    func testCompositeBottom_4_200_LowerThan_5_200() throws {
+    @Test func compositeBottom_4_200_LowerThan_5_200() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_4_200, ElevationRawTokens.elevationBottom_5_200)
     }
 
-    func testCompositeBottom_5_200_LowerThan_6_200() throws {
+    @Test func compositeBottom_5_200_LowerThan_6_200() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_5_200, ElevationRawTokens.elevationBottom_6_200)
     }
 
-    func testCompositeBottom_1_300_LowerThan_2_300() throws {
+    @Test func compositeBottom_1_300_LowerThan_2_300() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_1_300, ElevationRawTokens.elevationBottom_2_300)
     }
 
-    func testCompositeBottom_2_300_LowerThan_3_300() throws {
+    @Test func compositeBottom_2_300_LowerThan_3_300() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_2_300, ElevationRawTokens.elevationBottom_3_300)
     }
 
-    func testCompositeBottom_3_300_LowerThan_4_300() throws {
+    @Test func compositeBottom_3_300_LowerThan_4_300() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_3_300, ElevationRawTokens.elevationBottom_4_300)
     }
 
-    func testCompositeBottom_4_300_LowerThan_5_300() throws {
+    @Test func compositeBottom_4_300_LowerThan_5_300() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_4_300, ElevationRawTokens.elevationBottom_5_300)
     }
 
-    func testCompositeBottom_5_300_LowerThan_6_300() throws {
+    @Test func compositeBottom_5_300_LowerThan_6_300() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_5_300, ElevationRawTokens.elevationBottom_6_300)
     }
 
-    func testCompositeBottom_1_400_LowerThan_2_400() throws {
+    @Test func compositeBottom_1_400_LowerThan_2_400() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_1_400, ElevationRawTokens.elevationBottom_2_400)
     }
 
-    func testCompositeBottom_2_400_LowerThan_3_400() throws {
+    @Test func compositeBottom_2_400_LowerThan_3_400() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_2_400, ElevationRawTokens.elevationBottom_3_400)
     }
 
-    func testCompositeBottom_3_400_LowerThan_4_400() throws {
+    @Test func compositeBottom_3_400_LowerThan_4_400() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_3_400, ElevationRawTokens.elevationBottom_4_400)
     }
 
-    func testCompositeBottom_4_400_LowerThan_5_400() throws {
+    @Test func compositeBottom_4_400_LowerThan_5_400() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_4_400, ElevationRawTokens.elevationBottom_5_400)
     }
 
-    func testCompositeBottom_5_400_LowerThan_6_400() throws {
+    @Test func compositeBottom_5_400_LowerThan_6_400() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_5_400, ElevationRawTokens.elevationBottom_6_400)
     }
 
-    func testCompositeBottom_1_500_LowerThan_2_500() throws {
+    @Test func compositeBottom_1_500_LowerThan_2_500() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_1_500, ElevationRawTokens.elevationBottom_2_500)
     }
 
-    func testCompositeBottom_2_500_LowerThan_3_500() throws {
+    @Test func compositeBottom_2_500_LowerThan_3_500() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_2_500, ElevationRawTokens.elevationBottom_3_500)
     }
 
-    func testCompositeBottom_3_500_LowerThan_4_500() throws {
+    @Test func compositeBottom_3_500_LowerThan_4_500() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_3_500, ElevationRawTokens.elevationBottom_4_500)
     }
 
-    func testCompositeBottom_4_500_LowerThan_5_500() throws {
+    @Test func compositeBottom_4_500_LowerThan_5_500() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_4_500, ElevationRawTokens.elevationBottom_5_500)
     }
 
-    func testCompositeBottom_5_500_LowerThan_6_500() throws {
+    @Test func compositeBottom_5_500_LowerThan_6_500() throws {
         assertCompositeLowerThan(ElevationRawTokens.elevationBottom_5_500, ElevationRawTokens.elevationBottom_6_500)
     }
 
@@ -1173,6 +1171,5 @@ final class ElevationRawTokensTests: XCTestCase {
     }
 }
 
-// swiftlint:enable required_deinit
 // swiftlint:enable type_body_length
 // swiftlint:enable function_body_length
