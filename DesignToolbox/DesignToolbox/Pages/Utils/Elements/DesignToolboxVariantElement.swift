1dd458e64797ee093844b57df3466d34a7e43845
Ludovic PINEL <ludovic.pinel@orange.com>
2025-03-04 14:41:08 +0100

    Fix multiline text alignment in variant component page

--- DesignToolbox/DesignToolbox/Pages/Utils/Elements/DesignToolboxVariantElement.swift
+++ DesignToolbox/DesignToolbox/Pages/Utils/Elements/DesignToolboxVariantElement.swift
@@ -33,6 +33,7 @@ struct DesignToolboxVariantElement: View {
             } label: {
                 HStack {
                     Text(LocalizedStringKey(element.name))
+                        .multilineTextAlignment(.leading)
                         .typeHeadingMedium(theme)
                         .foregroundStyle(theme.colors.colorContentDefault.color(for: colorScheme))
                         .padding(.vertical, theme.spaces.spaceFixedShorter)
