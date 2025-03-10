54d09be1e7895e5d22d22f654f004d878c79d8a2
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2025-03-10 12:48:13 +0100

    chore: apply checkbox component tokens update (v0.7.0) (#486)
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- OUDS/Core/Themes/Orange/Tests/Values/ComponentTokens/MockTheme/MockTheme+AllCheckboxComponentTokens.swift
+++ OUDS/Core/Themes/Orange/Tests/Values/ComponentTokens/MockTheme/MockTheme+AllCheckboxComponentTokens.swift
@@ -27,17 +27,10 @@ final class MockThemeCheckboxComponentTokenProvider: OrangeThemeCheckboxComponen
     static let mockThemeCheckboxSize: SizeSemanticToken = 118_218
     static let mockThemeCheckboxBorderRadius: BorderRadiusSemanticToken = 1_312
     static let mockThemeCheckboxBorderWidth: BorderRadiusSemanticToken = 666
-    static let mockThemeCheckboxOpacity: OpacitySemanticToken = 0.007
-    static let mockThemeCheckboxColor = MultipleColorSemanticTokens("#FF0000")
 
     override public init(sizes: AllSizeSemanticTokensProvider?,
-                         borders: AllBorderSemanticTokensProvider?,
-                         colors: AllColorSemanticTokensProvider?,
-                         opacities: AllOpacitySemanticTokensProvider?) {
-        super.init(sizes: sizes,
-                   borders: borders,
-                   colors: colors,
-                   opacities: opacities)
+                         borders: AllBorderSemanticTokensProvider?) {
+        super.init(sizes: sizes, borders: borders)
     }
 
     // MARK: - Badge component tokens
@@ -47,7 +40,6 @@ final class MockThemeCheckboxComponentTokenProvider: OrangeThemeCheckboxComponen
     override public var checkboxSizeMinWidth: SizeSemanticToken { Self.mockThemeCheckboxSize }
     override public var checkboxSizeMaxHeight: SizeSemanticToken { Self.mockThemeCheckboxSize }
     override public var checkboxSizeMinHeight: SizeSemanticToken { Self.mockThemeCheckboxSize }
-    override public var checkboxSizeIndicatorInnerIcon: SizeSemanticToken { Self.mockThemeCheckboxSize }
     override public var checkboxSizeIndicator: SizeSemanticToken { Self.mockThemeCheckboxSize }
 
     // MARK: - Borders
@@ -61,30 +53,6 @@ final class MockThemeCheckboxComponentTokenProvider: OrangeThemeCheckboxComponen
     override public var checkboxBorderWidthUnselectedHover: BorderWidthSemanticToken { Self.mockThemeCheckboxBorderWidth }
     override public var checkboxBorderWidthUnselectedPressed: BorderWidthSemanticToken { Self.mockThemeCheckboxBorderWidth }
     override public var checkboxBorderWidthUnselectedFocus: BorderWidthSemanticToken { Self.mockThemeCheckboxBorderWidth }
-
-    // MARK: - Opacities
-
-    override public var checkboxOpacityBgSelected: OpacitySemanticToken { Self.mockThemeCheckboxOpacity }
-    override public var checkboxOpacityBgUnselected: OpacitySemanticToken { Self.mockThemeCheckboxOpacity }
-    override public var checkboxOpacityBgSelectedHover: OpacitySemanticToken { Self.mockThemeCheckboxOpacity }
-    override public var checkboxOpacityBgUnselectedHover: OpacitySemanticToken { Self.mockThemeCheckboxOpacity }
-    override public var checkboxOpacityBgSelectedPressed: OpacitySemanticToken { Self.mockThemeCheckboxOpacity }
-    override public var checkboxOpacityBgUnselectedPressed: OpacitySemanticToken { Self.mockThemeCheckboxOpacity }
-    override public var checkboxOpacityBgSelectedFocus: OpacitySemanticToken { Self.mockThemeCheckboxOpacity }
-    override public var checkboxOpacityBgUnselectedFocus: OpacitySemanticToken { Self.mockThemeCheckboxOpacity }
-
-    // MARK: - Colors
-
-    override public var checkboxColorContentSelected: MultipleColorSemanticTokens { Self.mockThemeCheckboxColor }
-    override public var checkboxColorContentEnabled: MultipleColorSemanticTokens { Self.mockThemeCheckboxColor }
-    override public var checkboxColorContentHover: MultipleColorSemanticTokens { Self.mockThemeCheckboxColor }
-    override public var checkboxColorContentPressed: MultipleColorSemanticTokens { Self.mockThemeCheckboxColor }
-    override public var checkboxColorContentDisabled: MultipleColorSemanticTokens { Self.mockThemeCheckboxColor }
-    override public var checkboxColorContentFocus: MultipleColorSemanticTokens { Self.mockThemeCheckboxColor }
-    override public var checkboxColorContentErrorEnabled: MultipleColorSemanticTokens { Self.mockThemeCheckboxColor }
-    override public var checkboxColorContentErrorHover: MultipleColorSemanticTokens { Self.mockThemeCheckboxColor }
-    override public var checkboxColorContentErrorPressed: MultipleColorSemanticTokens { Self.mockThemeCheckboxColor }
-    override public var checkboxColorContentErrorFocus: MultipleColorSemanticTokens { Self.mockThemeCheckboxColor }
 }
 
 // swiftlint:enable required_deinit
