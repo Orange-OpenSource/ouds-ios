06c3c7a036593ac9bca7b07ce37100cc4a675149
Ludovic PINEL <ludovic.pinel@orange.com>
2025-03-28 10:35:01 +0100

    refacto remove: divider if the controitem is outlined - rollback

--- OUDS/Core/Components/Sources/Internal/ControlItem/ControlItemContent.swift
+++ OUDS/Core/Components/Sources/Internal/ControlItem/ControlItemContent.swift
@@ -51,7 +51,7 @@ struct ControlItemContent: View {
             }
         }
         .padding(.all, theme.controlItem.controlItemSpaceInset)
-        .oudsDivider(show: showDivider)
+        .oudsDivider(show: layoutData.hasDivider)
         .background(backgroundColor)
         .modifier(ControlItemContentOutlinedModifier(interactionState: interactionState, layoutData: layoutData, isOn: isOn))
     }
@@ -71,11 +71,6 @@ struct ControlItemContent: View {
     }
 
     // MARK: Computed properties
-
-    private var showDivider: Bool {
-        // The divider could be displayed only if it is requested and the item is not outlined
-        layoutData.hasDivider && !layoutData.isOutlined
-    }
 
     private var isOn: Bool {
         switch indicatorType {
