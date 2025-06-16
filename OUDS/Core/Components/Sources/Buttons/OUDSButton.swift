932f07fc57db44eadcb764aecfb4419b25fa31d4
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2025-06-16 17:11:49 +0200

    refactor: disable loading button animation in low power mode (#661) (#710)
    Reviewed-by: Ludovic Pinel <ludovic.pinel@orange.com>
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- OUDS/Core/Components/Sources/Buttons/OUDSButton.swift
+++ OUDS/Core/Components/Sources/Buttons/OUDSButton.swift
@@ -66,6 +66,11 @@ import SwiftUI
 ///
 /// **Remark: Today it is not allowed to place a Negative button on a colored surface.**
 ///
+/// ## Specific behavior
+///
+/// For accessibility reasons, if the system settings for reducing motion is enabled, the loading indicator does not move.
+/// For ecodesign reasons, if the device is in low power mode, the loading indicator does not move.
+///
 /// ## Design documentation
 ///
 /// [unified-design-system.orange.com](https://unified-design-system.orange.com/472794e18/p/48a788-button)
