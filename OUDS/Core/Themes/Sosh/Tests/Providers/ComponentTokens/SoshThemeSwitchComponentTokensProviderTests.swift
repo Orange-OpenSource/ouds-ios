7953fbfad825d248be0b5b81c23820fd10825203
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2025-07-07 17:31:41 +0200

    test: update unit tests for Sosh objects (#665)
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

---
+++ OUDS/Core/Themes/Sosh/Tests/Providers/ComponentTokens/SoshThemeSwitchComponentTokensProviderTests.swift
@@ -0,0 +1,51 @@
+//
+// Software Name: OUDS iOS
+// SPDX-FileCopyrightText: Copyright (c) Orange SA
+// SPDX-License-Identifier: MIT
+//
+// This software is distributed under the MIT license,
+// the text of which is available at https://opensource.org/license/MIT/
+// or see the "LICENSE" file for more details.
+//
+// Authors: See CONTRIBUTORS.txt
+// Software description: A SwiftUI components library with code examples for Orange Unified Design System
+//
+
+@testable import OUDSThemesSosh
+import Testing
+
+// swiftlint:disable type_name
+
+/// Check if the component tokens provider under testsh as the default semantic token providers with the expected types.
+struct SoshThemeSwitchComponentTokensProviderTests {
+
+    private var provider: SoshThemeSwitchComponentTokensProvider
+
+    init() {
+        provider = SoshThemeSwitchComponentTokensProvider()
+    }
+
+    // MARK: - Tests
+
+    @Test func defaultSizeSemanticTokensProvider() throws {
+        #expect(provider.sizes is SoshThemeSizeSemanticTokensProvider)
+    }
+
+    @Test func defaultBorderSemanticTokensProvider() throws {
+        #expect(provider.borders is SoshThemeBorderSemanticTokensProvider)
+    }
+
+    @Test func defaultColorSemanticTokensProvider() throws {
+        #expect(provider.colors is SoshThemeColorSemanticTokensProvider)
+    }
+
+    @Test func defaultSpaceSemanticTokensProvider() throws {
+        #expect(provider.spaces is SoshThemeSpaceSemanticTokensProvider)
+    }
+
+    @Test func defaultOpacitySemanticTokensProvider() throws {
+        #expect(provider.opacities is SoshThemeOpacitySemanticTokensProvider)
+    }
+}
+
+// swiftlint:enable type_name
