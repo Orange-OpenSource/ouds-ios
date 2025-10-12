6942e1e5538da69167bff48f3e61a1ee4fc84654
chetan-kmr-0607 <118420797+chetan-kmr-0607@users.noreply.github.com>
2025-10-12 05:05:29 -0700

    fix: disable OUDSButton during loading state (#988)

--- OUDS/Core/Components/Sources/Actions/Button/OUDSButton.swift
+++ OUDS/Core/Components/Sources/Actions/Button/OUDSButton.swift
@@ -225,6 +225,7 @@ public struct OUDSButton: View {
             }
         }
         .buttonStyle(OUDSButtonStyle(isHover: isHover, appearance: appearance, style: style))
+                .disabled(style == .loading)
         .accessibilityLabel(accessibilityLabel)
         .onHover { isHover in
             self.isHover = isHover
