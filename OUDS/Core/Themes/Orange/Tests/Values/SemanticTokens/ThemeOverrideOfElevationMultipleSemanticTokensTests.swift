683fda62f26f86a8b2c9caa99af9239a8353191f
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2025-10-14 09:12:59 +0200

    refactor: elevation multiple semantic tokens 5#1031)
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

---
+++ OUDS/Core/Themes/Orange/Tests/Values/SemanticTokens/ThemeOverrideOfElevationMultipleSemanticTokensTests.swift
@@ -0,0 +1,62 @@
+//
+// Software Name: OUDS iOS
+// SPDX-FileCopyrightText: Copyright (c) Orange SA
+// SPDX-License-Identifier: MIT
+//
+// This software is distributed under the MIT license,
+// the text of which is available at https://opensource.org/license/MIT/
+// or see the "LICENSE" file for more details.
+//
+// Authors: See CONTRIBUTORS.txt
+// Software description: A SwiftUI components library with code examples for Orange Unified Design System
+//
+
+import OUDS
+import OUDSThemesOrange
+import Testing
+
+// swiftlint:disable type_name
+
+/// The architecture of _OUDS iOS_ _Swift package_ library is based on _object oriented paradigm_ and overriding of classes.
+/// In fact, the `OUDSTheme` object is a class, which can be seen as an _asbtract class_, exposing through its extensions and protocols _elevation multiple semantic tokens_.
+/// These semantic tokens should be overridable by subclasses like the ``OrangeThemeElevationMultipleSemanticTokensProvider``.
+///
+/// **These tests checks if any _elevation multiple semantic semantic tokens_ can be surcharged by a child theme**
+/// **Also, it can help to find removed or renamed tokens by having tests no more compilable**
+struct ThemeOverrideOfElevationMultipleSemanticTokensTests {
+
+    private var abstractTheme: OUDSTheme
+    private var inheritedTheme: OUDSTheme
+
+    init() {
+        abstractTheme = OrangeTheme()
+        inheritedTheme = MockTheme()
+    }
+
+    @Test func inheritedThemeCanOverrideSemanticTokenElevationColorDefault() throws {
+        #expect(inheritedTheme.elevations.elevationColorDefault != abstractTheme.elevations.elevationColorDefault)
+        #expect(inheritedTheme.elevations.elevationColorDefault == MockThemeElevationSemanticTokensProvider.mockThemeElevationMultipleColorSemanticToken)
+    }
+
+    @Test func inheritedThemeCanOverrideSemanticTokenElevationColorRaised() throws {
+        #expect(inheritedTheme.elevations.elevationColorRaised != abstractTheme.elevations.elevationColorRaised)
+        #expect(inheritedTheme.elevations.elevationColorRaised == MockThemeElevationSemanticTokensProvider.mockThemeElevationMultipleColorSemanticToken)
+    }
+
+    @Test func inheritedThemeCanOverrideSemanticTokenElevationColorDrag() throws {
+        #expect(inheritedTheme.elevations.elevationColorDrag != abstractTheme.elevations.elevationColorDrag)
+        #expect(inheritedTheme.elevations.elevationColorDrag == MockThemeElevationSemanticTokensProvider.mockThemeElevationMultipleColorSemanticToken)
+    }
+
+    @Test func inheritedThemeCanOverrideSemanticTokenElevationColorEmphasized() throws {
+        #expect(inheritedTheme.elevations.elevationColorEmphasized != abstractTheme.elevations.elevationColorEmphasized)
+        #expect(inheritedTheme.elevations.elevationColorEmphasized == MockThemeElevationSemanticTokensProvider.mockThemeElevationMultipleColorSemanticToken)
+    }
+
+    @Test func inheritedThemeCanOverrideSemanticTokenElevationColorSticky() throws {
+        #expect(inheritedTheme.elevations.elevationColorSticky != abstractTheme.elevations.elevationColorSticky)
+        #expect(inheritedTheme.elevations.elevationColorSticky == MockThemeElevationSemanticTokensProvider.mockThemeElevationMultipleColorSemanticToken)
+    }
+}
+
+// swiftlint:enable type_name
