7f7804298755b41d97b28393f545fd28b4a95d0b
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2025-10-19 04:53:04 +0200

    docs: add chapter 5 for tutorial (#919)
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

---
+++ OUDS/Core/OUDS/Sources/_OUDS.docc/Tutorial/swift-samples/tutorial-5-9.swift
@@ -0,0 +1,21 @@
+import OUDS
+import OUDSComponents
+import OUDSFoundations
+import OUDSThemesOrange
+import OUDSTokensRaw
+import OUDSTokensSemantic
+import SwiftUI
+
+@main
+struct OUDSTutorialSandboxApp: App {
+
+    let myTheme = OrangeTheme(fontFamily: "Menlo", tuning: Tuning(hasRoundedButtons: true, hasRoundedTextInputs: true))
+
+    var body: some Scene {
+        WindowGroup {
+            OUDSThemeableView(theme: myTheme) {
+                ContentView()
+            }
+        }
+    }
+}
