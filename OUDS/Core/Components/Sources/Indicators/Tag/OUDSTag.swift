20ddbccbff5e49e1f80a3675111571677fc1b3d1
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2026-01-09 15:12:46 +0100

    docs: update definition for tags (#1238)
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- OUDS/Core/Components/Sources/Indicators/Tag/OUDSTag.swift
+++ OUDS/Core/Components/Sources/Indicators/Tag/OUDSTag.swift
@@ -15,8 +15,8 @@ import SwiftUI
 
 // swiftlint:disable nesting
 
-/// A tag is a small element that shows short information like a label, keyword, or category.
-/// It helps users quickly find, group, or understand content.
+/// Tag is a UI element that allows to display short info like a label, keyword, or category.
+/// Tag helps users quickly find, group, or understand content.
 ///
 /// ## Appearances
 ///
