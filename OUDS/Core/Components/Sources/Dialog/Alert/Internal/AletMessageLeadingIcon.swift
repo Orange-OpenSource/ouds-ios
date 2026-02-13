8ae954c6b8fe7d092e1994a6e03c92d4e5379f3a
Ludovic PINEL <ludovic.pinel@orange.com>
2026-02-13 10:46:06 +0100

    refactor: The leading icon to use two asset for warning

--- OUDS/Core/Components/Sources/Dialog/Alert/Internal/AletMessageLeadingIcon.swift
+++ OUDS/Core/Components/Sources/Dialog/Alert/Internal/AletMessageLeadingIcon.swift
@@ -20,63 +20,53 @@ struct AletMessageLeadingIcon: View {
 
     let status: OUDSAlertMessage.Status
     @Environment(\.theme) private var theme
+    @Environment(\.dynamicTypeSize) private var dynamicTypeSize: DynamicTypeSize
 
     // MARK: - Body
 
     var body: some View {
-        if let icon {
-            ScaledIcon(icon: icon.renderingMode(.template),
-                       size: theme.alert.sizeIcon)
-                .oudsForegroundColor(color)
-                .toFlip(flipIcon)
-                .padding(.top, theme.alert.spacePaddingBlock)
+        HStack {
+            switch status {
+            case let .accent(icon, flipIcon):
+                icon.update(with: theme.colors.contentStatusAccent, flipped: flipIcon)
+            case let .neutral(icon, flipIcon):
+                icon.update(with: theme.icon.colorContentDefault, flipped: flipIcon)
+            case let .negative(showIcon) where showIcon == true:
+                Image("ic_alert_important_fill", bundle: theme.resourcesBundle)
+                    .update(with: theme.colors.contentStatusNegative)
+            case let .positive(showIcon) where showIcon == true:
+                Image("ic_alert_tick_confirmation_fill", bundle: theme.resourcesBundle)
+                    .update(with: theme.colors.contentStatusPositive)
+            case let .info(showIcon) where showIcon == true:
+                Image("ic_alert_info_fill", bundle: theme.resourcesBundle)
+                    .update(with: theme.colors.contentStatusInfo)
+            case let .warning(showIcon) where showIcon == true:
+                ZStack {
+                    Image(decorative: "ic_alert_warning_external_shape", bundle: theme.resourcesBundle)
+                        .update(with: theme.icon.colorContentStatusWarningExternalShape)
+                    Image(decorative: "ic_alert_warning_internal_shape", bundle: theme.resourcesBundle)
+                        .update(with: theme.icon.colorContentStatusWarningInternalShape)
+                }
+            default:
+                EmptyView()
+            }
         }
+        .frame(width: size, height: size)
+        .padding(.top, theme.alert.spacePaddingBlock)
     }
 
-    private var color: MultipleColorSemanticToken {
-        switch status {
-        case .accent:
-            theme.colors.contentStatusAccent
-        case .neutral:
-            theme.colors.contentDefault
-        case .positive:
-            theme.colors.contentStatusPositive
-        case .info:
-            theme.colors.contentStatusInfo
-        case .warning:
-            theme.colors.contentStatusWarning
-        case .negative:
-            theme.colors.contentStatusNegative
-        }
+    private var size: CGFloat {
+        let rawSize = theme.alert.sizeIcon
+        return rawSize * dynamicTypeSize.percentageRate / 100
     }
+}
 
-    private var icon: Image? {
-        switch status {
-        case let .accent(icon, _):
-            icon
-        case let .neutral(icon, _):
-            icon
-        case let .negative(showIcon) where showIcon == true:
-            Image("ic_alert_important_fill", bundle: theme.resourcesBundle)
-        case let .positive(showIcon) where showIcon == true:
-            Image("ic_alert_tick_confirmation_fill", bundle: theme.resourcesBundle)
-        case let .info(showIcon) where showIcon == true:
-            Image("ic_alert_info_fill", bundle: theme.resourcesBundle)
-        case let .warning(showIcon) where showIcon == true:
-            Image("ic_alert_warning_external_shape", bundle: theme.resourcesBundle)
-        default:
-            nil
-        }
-    }
-
-    private var flipIcon: Bool {
-        switch status {
-        case let .accent(_, flipIcon):
-            flipIcon
-        case let .neutral(_, flipIcon):
-            flipIcon
-        default:
-            false
-        }
+extension Image {
+    @MainActor
+    func update(with color: MultipleColorSemanticToken, flipped: Bool = false) -> some View {
+        resizable()
+            .renderingMode(.template)
+            .toFlip(flipped)
+            .oudsForegroundColor(color)
     }
 }
