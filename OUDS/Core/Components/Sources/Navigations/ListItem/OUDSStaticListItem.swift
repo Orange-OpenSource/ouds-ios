28e0bb936acfbb5c0fadc0657cef102033f4c6f4
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2026-06-17 14:28:35 +0200

    refactor: documentation, a11y trait, navigaiton link with bold label (#265)
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- OUDS/Core/Components/Sources/Navigations/ListItem/OUDSStaticListItem.swift
+++ OUDS/Core/Components/Sources/Navigations/ListItem/OUDSStaticListItem.swift
@@ -88,7 +88,7 @@ import SwiftUI
 ///
 /// - ``SwiftUICore/View/oudsListItemSize(_:)`` — Sets the size of the item (`.standard` or `.small`).
 /// - ``SwiftUICore/View/oudsListItemContainerAlignment(_:)`` — Aligns leading, trailing, and text containers (`.top` or `.center`).
-/// - ``SwiftUICore/View/oudsListItemStyle(style:)`` — Applies a standard style with optional divider and background.
+/// - ``SwiftUICore/View/oudsListItemStyle(_:)`` — Applies a standard style with optional divider and background.
 /// - ``SwiftUICore/View/oudsListCardStyle(hasDivider:hasBackground:)`` — Applies a card style on list items.
 /// - ``SwiftUICore/View/oudsListItemRoundedMedia(_:)`` — Defines whether media (images, videos) should be displayed with rounded corners.
 ///
