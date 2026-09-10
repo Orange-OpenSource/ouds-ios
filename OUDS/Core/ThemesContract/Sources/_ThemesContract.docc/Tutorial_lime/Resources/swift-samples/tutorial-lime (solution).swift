c99dab7e585c2ce70a4697a1c65212e55d0015b8
Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>
2026-09-10 15:09:58 +0200

    chore: clean sources (#1547)
    Signed-off-by: Pierre-Yves Lapersonne <pierreyves.lapersonne@orange.com>

--- OUDS/Core/ThemesContract/Sources/_ThemesContract.docc/Tutorial_lime/Resources/swift-samples/tutorial-lime (solution).swift
+++ OUDS/Core/ThemesContract/Sources/_ThemesContract.docc/Tutorial_lime/Resources/swift-samples/tutorial-lime (solution).swift
@@ -1,3 +1,16 @@
+//
+// Software Name: OUDS iOS
+// SPDX-FileCopyrightText: Copyright (c) Orange SA
+// SPDX-License-Identifier: MIT
+//
+// This software is distributed under the MIT license,
+// the text of which is available at https://opensource.org/license/MIT/
+// or see the "LICENSE" file for more details.
+//
+// Authors: See CONTRIBUTORS.txt
+// Software description: A SwiftUI components library with code examples for Orange Unified Design System
+//
+
 // The umbrella imports to have all OUDS products
 // Get more details about imports here: https://ios.unified-design-system.orange.com/documentation/oudsthemescontract/gettingstarted
 import OUDSSwiftUI
